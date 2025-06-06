c589fd9675a6901ffcb50a37d0d249e19bfc5041
InvestedBrick <julianbrecht25@gmail.com>
2025-06-06 19:12:06 +0200

    test

--- src/io.c
+++ src/io.c
@@ -4,5 +4,5 @@ void serial_configure_baud_rate(unsigned short com, unsigned short divisor){
     outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
     outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00ff);
     outb(SERIAL_DATA_PORT(com), divisor & 0x00ff);
-    ' test'
+    // test
 }
