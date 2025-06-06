f5fe632c9d88c8b0d6e729c5fca56a314bfca8cc
InvestedBrick <julianbrecht25@gmail.com>
2025-06-07 00:22:02 +0200

    Removed files

--- src/io.c
+++
@@ -1,8 +0,0 @@
-#include "io.h"
-
-void serial_configure_baud_rate(unsigned short com, unsigned short divisor){
-    outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
-    outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00ff);
-    outb(SERIAL_DATA_PORT(com), divisor & 0x00ff);
-    // test
-}
