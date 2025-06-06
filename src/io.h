aa104601f631e47b20b63e58c68efd6aef72c48a
InvestedBrick <julianbrecht25@gmail.com>
2025-06-06 19:09:34 +0200

    Added some serial config

--- src/io.h
+++ src/io.h
@@ -3,11 +3,28 @@
 #ifndef INCLUDE_IO_H
 #define INCLUDE_IO_H
 
-#define FB_COMMAND_PORT         0x3D4
-#define FB_DATA_PORT            0x3D5
+/* SCREEN I/O */
+#define FB_COMMAND_PORT                 0x3D4
+#define FB_DATA_PORT                    0x3D5
+   
+#define FB_HIGH_BYTE_COMMAND            14
+#define FB_LOW_BYTE_COMMAND             15
 
-#define FB_HIGH_BYTE_COMMAND    14
-#define FB_LOW_BYTE_COMMAND     15
+/* Serial I/O */
+
+#define SERIAL_COM1_BASE                0x3f8
+   
+#define SERIAL_DATA_PORT(base)          (base)
+#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+
+/* SERIAL_LINE_ENABLE_DLAB:
+ * Tells the serial port to expect first the highest 8 bits on the data port,
+ * then the lowest 8 bits will follow
+ */
+#define SERIAL_LINE_ENABLE_DLAB         0x80
 
 /**
  * outb - sends byte to I/O port
@@ -25,4 +42,16 @@ void outb(unsigned short port, unsigned char data);
  * @return recieved byte
  */
 unsigned char inb(unsigned short port);
+
+/**
+ * serial_configure_baud_rate:
+ * sets speed of data being sent
+ * base speed is 115299 bits/s
+ * resulting speed is (115299 / divisor) bits/s
+ * 
+ * @param com The COM port to configure
+ * @param divisor The divisor
+ */
+
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor);
 #endif
