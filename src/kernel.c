bcff98c9f54d7ef8f71021946b19f006988f8232
InvestedBrick <julianbrecht25@gmail.com>
2025-06-07 11:15:52 +0200

    added log testing

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,6 @@
+#include "io.h"
+#include "log.h"
 #include "screen.h"
-
 
 void kmain(void)
 {
@@ -7,5 +8,14 @@ void kmain(void)
     clear_screen();
     write_string(msg,15);
 
+    // Serial port setup
+    serial_configure_baud_rate(SERIAL_COM1_BASE,3);
+    serial_configure_line(SERIAL_COM1_BASE);
+    serial_configure_buffer(SERIAL_COM1_BASE);
+    serial_configure_modem(SERIAL_COM1_BASE);
+
+    log("Hello Log");
+    warn("Watch out!");
+    error("FAILED");
     return;
 }
