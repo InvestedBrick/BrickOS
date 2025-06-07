e1a63c5b6c3dc46f59f7897058c876a60e26529a
InvestedBrick <julianbrecht25@gmail.com>
2025-06-07 15:46:13 +0200

    global descriptor table initialization

--- src/tables/gdt_setup.c
+++ src/tables/gdt_setup.c
@@ -1,7 +1,7 @@
 #include "gdt.h"
 
-struct gdt_entry gdt_entries[3];
-struct gdt gdtp;
+gdt_entry_t gdt_entries[3];
+gdt_t gdtp;
 void set_gdt_entry(int num,unsigned int base, unsigned int limit, unsigned char access, unsigned char gran){
     gdt_entries[num].base_low = (base & 0xffff);
     gdt_entries[num].base_middle = (base >> 16) & 0xff;
@@ -12,12 +12,12 @@ void set_gdt_entry(int num,unsigned int base, unsigned int limit, unsigned char 
 }
 
 void init_gdt() {
-    gdtp.size = (sizeof(struct gdt_entry) * 3 ) - 1;
+    gdtp.size = (sizeof(gdt_entry_t) * 3 ) - 1;
     gdtp.address = (unsigned int) &gdt_entries;
 
     set_gdt_entry(0,0,0,0,0); // null segment
     set_gdt_entry(1,0,0xffffffff,0x9a,0xcf); // Code segment
     set_gdt_entry(2,0,0xffffffff,0x92,0xcf); // Data segment
 
-    load_gdt((struct gdt*)&gdtp);
+    load_gdt((gdt_t*)&gdtp);
 }
