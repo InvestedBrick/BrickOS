1bc82a1f6eb6232f6412a96a92f542b58e016fef
InvestedBrick <julianbrecht25@gmail.com>
2025-06-07 15:44:38 +0200

    Serial logging headers

--- src/io/log.h
+++ src/io/log.h
@@ -5,36 +5,24 @@
 
 #include "io.h"
 
-void serial_write_with_prefix(const unsigned char* prefix, const unsigned char* msg,unsigned short com){
-    serial_write(prefix,com);
-    serial_write(msg,com);
-    serial_write("\n",com);
-}
+void serial_write_with_prefix(const unsigned char* prefix, const unsigned char* msg,unsigned short com);
 
 /**
  * log - logs a message to the COM1 serial port
  * @param msg The message to log 
  */
-void log(const unsigned char* msg){
-    serial_write_with_prefix("[LOG]   ",msg,SERIAL_COM1_BASE);
-}
+void log(const unsigned char* msg);
 
 /**
  * warn - logs a warning to the COM1 serial port
  * @param msg The warning
  */
-void warn(const unsigned char* msg){
-    serial_write_with_prefix("[WARN]  ",msg,SERIAL_COM1_BASE);
-
-}
+void warn(const unsigned char* msg);
 
 /**
  * error - logs an error to the COM1 serial port
  * @param msg The error
  */
-void error(const unsigned char* msg){
-    serial_write_with_prefix("[ERROR] ",msg,SERIAL_COM1_BASE);
-
-}
+void error(const unsigned char* msg);
 
 #endif
