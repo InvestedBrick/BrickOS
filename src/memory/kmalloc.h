defb9f8f5c05c9f8626710e83f3960ccc10a5845
InvestedBrick <julianbrecht25@gmail.com>
2025-06-09 16:12:41 +0200

    Basic kmalloc /kfree implementation

--- src/memory/kmalloc.h
+++ src/memory/kmalloc.h
@@ -3,6 +3,13 @@
 #ifndef INCLUDE_KMALLOC_H
 #define INCLUDE_KMALLOC_H
 
+typedef struct{
+    unsigned int size;
+    memory_block_t* next;
+    unsigned char free;
+}__attribute__((packed)) memory_block_t;
+#define MEMORY_BLOCK_SIZE sizeof(memory_block_t)
+
 /**
  * init_kmalloc:
  * initializes the kernel memory allocator
@@ -18,4 +25,18 @@ void init_kmalloc(unsigned int initial_heapsize);
  * @param new_size The new heapsize
  */
 void set_heap_size(unsigned int new_size);
+/**
+ * kmalloc:
+ * Allocates some memory for the kernel
+ * 
+ * @param size The number of bytes to allocate
+ * @return A pointer to the start of the allocated memory region
+ */
+void* kmalloc(unsigned int size);
+/**
+ * kfree:
+ * Frees allocated kernel memory
+ * @param addr The pointer to the memory location 
+ */
+void kfree(void* addr);
 #endif
