28aad96e8b044bfe3967b596521f6bf1e862ca45
InvestedBrick <julianbrecht25@gmail.com>
2025-06-10 17:02:19 +0200

    Added User mode segments

--- src/tables/gdt_setup.c
+++ src/tables/gdt_setup.c
@@ -1,6 +1,6 @@
 #include "gdt.h"
 
-gdt_entry_t gdt_entries[3];
+gdt_entry_t gdt_entries[N_GDT_ENTRIES];
 gdt_t gdtp;
 void set_gdt_entry(int num,unsigned int base, unsigned int limit, unsigned char access, unsigned char gran){
     gdt_entries[num].base_low = (base & 0xffff);
@@ -12,12 +12,20 @@ void set_gdt_entry(int num,unsigned int base, unsigned int limit, unsigned char 
 }
 
 void init_gdt() {
-    gdtp.size = (sizeof(gdt_entry_t) * 3 ) - 1;
+    gdtp.size = (sizeof(gdt_entry_t) * N_GDT_ENTRIES ) - 1;
     gdtp.address = (unsigned int) &gdt_entries;
+    //        P DPL S E DC RW A
+    // 0x9a = 1 00  1 1 0  1  0
+    // 0x92 = 1 00  1 0 0  1  0
 
+    //USR EX  1 11  1 1 0  1  0
+    //USR DT  1 11  1 0 0  1  0
     set_gdt_entry(0,0,0,0,0); // null segment
-    set_gdt_entry(1,0,0xffffffff,0x9a,0xcf); // Code segment
-    set_gdt_entry(2,0,0xffffffff,0x92,0xcf); // Data segment
+    set_gdt_entry(1,0,0xffffffff,KERNEL_CODE_SEGMENT,GRANULARITY); // Code segment
+    set_gdt_entry(2,0,0xffffffff,KERNEL_DATA_SEGMENT,GRANULARITY); // Data segment
+
+    set_gdt_entry(3,0,0xffffffff,USER_CODE_SEGMENT,GRANULARITY);
+    set_gdt_entry(4,0,0xffffffff,USER_DATA_SEGNENT,GRANULARITY);
 
     load_gdt((gdt_t*)&gdtp);
 }
