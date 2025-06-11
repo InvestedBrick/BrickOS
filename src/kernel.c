4260eb5c9e4f9e915e3756cce252cdde36697262
InvestedBrick <julianbrecht25@gmail.com>
2025-06-12 00:03:19 +0200

    IT FUCKING WORKS

--- src/kernel.c
+++ src/kernel.c
@@ -10,18 +10,13 @@
 #include "user/user_process.h"
 #include "tables/tss.h"
 #include "util.h"
+#include "modules/module_handler.h"
 
-char program[MEMORY_PAGE_SIZE] = {0};
 
 void kmain(multiboot_info_t* boot_info)
 {   
     const char* msg = "Hello BrickOS!";
-    unsigned int n_mods = boot_info->mods_count;
    
-    multiboot_module_t* modules = (multiboot_module_t*) (boot_info->mods_addr);
-    unsigned int size = modules[0].mod_end - modules[0].mod_start;
-    memcpy(program,(char*)modules[0].mod_start,size);
-
     clear_screen();
     write_string(msg,15);
 
@@ -46,36 +41,31 @@ void kmain(multiboot_info_t* boot_info)
 
     // calculate memory region
     unsigned int mod1 = *(unsigned int*) (boot_info->mods_addr + 4);
-    unsigned int physical_alloc_start = (mod1 - 0xfff) & ~ 0xfff;
+    unsigned int physical_alloc_start = (mod1 + 0xfff) & ~ 0xfff;
 
-    init_memory(physical_alloc_start,boot_info->mem_upper);
+    init_memory(physical_alloc_start ,boot_info->mem_upper * 1024);
     log("Initialized paged memory");
     
     // Set up kernel malloc
     init_kmalloc(MEMORY_PAGE_SIZE);
     log("Initialized kmalloc");
     
+    save_module_binaries(boot_info);
+
+    un_identity_map_first_page_table();
+
     init_user_process_vector();
     log("Initialized user process vector");
     
-    //multiboot_module_t* modules = (multiboot_module_t*) (boot_info->mods_addr);
-    //unsigned int mod_count = boot_info->mods_count;
 
-    if (n_mods > 0){
-        log("Found a module");
-        //unsigned char* binary = (unsigned char*) modules[0].mod_start;
-        //unsigned int size = modules[0].mod_end - modules[0].mod_start;
-        // set up teh user process and load the binary into memory
-        log("Before User process");
-        unsigned int pid = create_user_process(program,size);
-        log("Created User process");
+    if (module_count > 0){
+        unsigned int pid = create_user_process((char*)module_binary_structs[0].start,module_binary_structs[0].size);
+        free_saved_module_binaries();
         user_process_t* process = get_user_process_by_pid(pid);
         set_kernel_stack(process->kernel_stack + MEMORY_PAGE_SIZE);
-        log("Set up kernel stack");
         enter_user_mode(); // we are never getting out of here
         restore_kernel_memory_page_dir();
     } 
-    //un_identity_map_first_4MB(); // can no longer use boot info now
 
     //get input
     while(1){
