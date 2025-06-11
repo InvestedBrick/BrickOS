525c7f8f0f64b1ec481deec3a8ee32a80a2965e6
InvestedBrick <julianbrecht25@gmail.com>
2025-06-11 15:09:21 +0200

    useless function because I am too fuckign dumb to make this shit work

--- src/user/enter_user_mode.asm
+++ src/user/enter_user_mode.asm
@@ -4,7 +4,7 @@ USER_MODE_CODE_SEGMENT_SELECTOR equ 0x18 | 0x3
 USER_MODE_STACK_SEGMENT_SELECTOR equ 0x20 | 0x3
 USER_MODE_STACK_POINTER equ 0xbffffffb
 USER_MODE_INTRUC_POINTER equ 0x00000000
-USER_MODE_EFLAGS equ 0x0 ; no syscalls for now
+USER_MODE_EFLAGS equ (1 << 9)
 enter_user_mode:
     ;Stack for iret:
     ; [esp + 16] ss
