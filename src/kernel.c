c75802748f60d228701f452b622131e9e0455cbe
InvestedBrick <julianbrecht25@gmail.com>
2025-06-11 15:05:42 +0200

    Fuck this shit, not working, creating new branch

--- src/kernel.c
+++ src/kernel.c
@@ -8,10 +8,20 @@
 #include "memory/memory.h"
 #include "memory/kmalloc.h"
 #include "user/user_process.h"
+#include "tables/tss.h"
+#include "util.h"
+
+char program[MEMORY_PAGE_SIZE] = {0};
 
 void kmain(multiboot_info_t* boot_info)
 {   
     const char* msg = "Hello BrickOS!";
+    unsigned int n_mods = boot_info->mods_count;
+   
+    multiboot_module_t* modules = (multiboot_module_t*) (boot_info->mods_addr);
+    unsigned int size = modules[0].mod_end - modules[0].mod_start;
+    memcpy(program,(char*)modules[0].mod_start,size);
+
     clear_screen();
     write_string(msg,15);
 
@@ -46,7 +56,26 @@ void kmain(multiboot_info_t* boot_info)
     log("Initialized kmalloc");
     
     init_user_process_vector();
+    log("Initialized user process vector");
+    
+    //multiboot_module_t* modules = (multiboot_module_t*) (boot_info->mods_addr);
+    //unsigned int mod_count = boot_info->mods_count;
 
+    if (n_mods > 0){
+        log("Found a module");
+        //unsigned char* binary = (unsigned char*) modules[0].mod_start;
+        //unsigned int size = modules[0].mod_end - modules[0].mod_start;
+        // set up teh user process and load the binary into memory
+        log("Before User process");
+        unsigned int pid = create_user_process(program,size);
+        log("Created User process");
+        user_process_t* process = get_user_process_by_pid(pid);
+        set_kernel_stack(process->kernel_stack + MEMORY_PAGE_SIZE);
+        log("Set up kernel stack");
+        enter_user_mode(); // we are never getting out of here
+        restore_kernel_memory_page_dir();
+    } 
+    //un_identity_map_first_4MB(); // can no longer use boot info now
 
     //get input
     while(1){
