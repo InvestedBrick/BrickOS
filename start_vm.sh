9d842ee1caed98ea2291d3eed065a19086233cee
InvestedBrick <julianbrecht25@gmail.com>
2025-06-15 23:07:21 +0200

    Added debug info, didnt help sadly

--- start_vm.sh
+++ start_vm.sh
@@ -1,3 +1,3 @@
-qemu-system-i386 -drive file=hdd.img,format=raw,if=ide -serial file:serial.log -boot d -cdrom BrickOS.iso -m 512 
+qemu-system-i386 -d int -drive file=hdd.img,format=raw,if=ide -serial file:serial.log -boot d -cdrom BrickOS.iso -m 512 
 
 # add "-d int" as argument for more debug info 
