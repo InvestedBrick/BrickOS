22a90a9d6c62223bd914758e123838f1956b9f46
InvestedBrick <julianbrecht25@gmail.com>
2025-06-27 22:39:05 +0200

    Saving modules to files in the modules folder

--- src/modules/module_handler.c
+++ src/modules/module_handler.c
@@ -1,11 +1,14 @@
 #include "module_handler.h"
 #include "../memory/kmalloc.h"
 #include "../util.h"
-
+#include "../filesystem/filesystem.h"
+#include "../filesystem/file_operations.h"
+#include "../io/log.h"
 module_binary_t* module_binary_structs;
 unsigned int module_count;
 
 void save_module_binaries(multiboot_info_t* boot_info){
+    
     module_count = boot_info->mods_count;
     module_binary_structs = kmalloc(module_count * sizeof(module_binary_t));
 
@@ -13,7 +16,21 @@ void save_module_binaries(multiboot_info_t* boot_info){
 
     multiboot_module_t* modules = (multiboot_module_t*)boot_info->mods_addr;
     for (unsigned int i = 0; i < module_count;i++){
-        module_binary_structs[i].size = modules[0].mod_end - modules[0].mod_start;
+        module_binary_structs[i].size = modules[i].mod_end - modules[i].mod_start;
+        
+        unsigned char* cmd_line = (unsigned char*)modules[i].cmdline;
+        // looks like "/modules/<module name>"
+        unsigned int name_start_idx = find_char(&cmd_line[1],'/') + 2; // skip past the first '/' and return index to the first char of the name
+        
+        if (name_start_idx == (unsigned int)-1) 
+            {error("Finding name for module failed");}
+        else{
+            unsigned int mod_name_len = strlen(&cmd_line[name_start_idx]);
+            module_binary_structs[i].cmdline = (unsigned char*)kmalloc(mod_name_len + 1);
+            memcpy(module_binary_structs[i].cmdline,&cmd_line[name_start_idx],mod_name_len);
+            module_binary_structs[i].cmdline[mod_name_len] = '\0';
+            
+        }
 
         module_binary_structs[i].start = (unsigned int)kmalloc(module_binary_structs[i].size);
 
@@ -28,5 +45,42 @@ void free_saved_module_binaries(){
     for (unsigned int i = 0; i < module_count;i++){
         kfree((void*)module_binary_structs[i].start);
     }
+    kfree((void*)module_binary_structs);
+}
+
+void write_module_binaries_to_file(){
+    for (unsigned int i = 0; i < module_count;i++){
+        inode_t* module_dir = get_inode_by_full_file_path("root/modules/");
+        
+        inode_t* dir_save = change_active_dir(module_dir);
+        if (!module_dir) 
+            {error("Modules directory was not initialized"); return; }
+        
+        unsigned char* name = module_binary_structs[i].cmdline;
+        unsigned int len = strlen(name);
+        if (dir_contains_name(module_dir,name)){
+            // if it is already here, remove it
+            delete_file(module_dir,name,len);
+        }
+
+        int ret_code = create_file(module_dir,name,len,FS_TYPE_FILE);
+        if (ret_code < 0) 
+            {error("Failed to create module binary file"); continue;}
+
+        int fd = open(name,FILE_FLAG_WRITE);
+        if (fd == FILE_OP_FAILED) 
+            {error("Failed to open module binary file"); continue;}
+
+        ret_code = write(fd,(unsigned char*)module_binary_structs[i].start,module_binary_structs[i].size);
+
+        if (ret_code < 0) 
+            {error("Failed to write to module binary file"); continue;}
+        
+        ret_code = close(fd);
+        if (ret_code == FILE_INVALID_FD) error("Failed to close module binary file");
+
+        change_active_dir(dir_save);
+    }
+
     kfree((void*)module_binary_structs);
 }
