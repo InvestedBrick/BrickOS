5672bc4e31a27b8203b8b8503ba61b9710322d9d
InvestedBrick <julianbrecht25@gmail.com>
2025-06-28 17:06:53 +0200

    Moved vector code to own file

--- src/util.c
+++ src/util.c
@@ -1,7 +1,7 @@
 #include "util.h"
 #include "memory/kmalloc.h"
 #include "io/log.h"
-// TODO: move the vector stuff out of here
+
 void* memset(void* dest, int val, unsigned int n){
     unsigned char* p = dest;
     for(unsigned int i = 0; i < n;i++){
@@ -71,73 +71,6 @@ unsigned int strlen(unsigned char* str){
     unsigned int len = 0;
     while(str[len] != 0){len++;}
     return len;
-}
-
-void vector_resize(vector_t* vec,unsigned int new_capacity){
-    unsigned int* new_data = kmalloc(sizeof(unsigned int) * new_capacity);
-    memcpy(new_data,vec->data,vec->size * sizeof(unsigned int));
-    kfree(vec->data);
-
-    vec->data = new_data;
-    vec->capacity = new_capacity;
-}
-
-void vector_append(vector_t* vec, unsigned int data){
-    if (vec->size >= vec->capacity){
-        vector_resize(vec,vec->capacity * 2);
-    }
-
-    vec->data[vec->size] = data;
-    vec->size++;
-}
-unsigned int vector_pop(vector_t* vec){
-    if (!vector_is_empty(vec)){
-        if (vec->size - 1 <= vec->capacity * 0.25f){
-            vector_resize(vec,vec->capacity * 0.5);
-        }
-        vec->size--;
-        return vec->data[vec->size];
-    }
-    return 0;
-}
-
-unsigned char vector_is_empty(vector_t* vec) {
-    return vec->size == 0;
-}
-
-unsigned int vector_erase(vector_t* vec,unsigned int idx){
-    unsigned int erased_item = vec->data[idx];
-
-    for(unsigned int i = idx; i < vec->size - 1;i++){
-        vec->data[i] = vec->data[i + 1];
-    }
-
-    vec->size--;
-    return erased_item;
-}
-
-unsigned int vector_find(vector_t* vec, unsigned int data){
-    for(unsigned int i = 0; i < vec->size;i++){
-        if (vec->data[i] == data) return i;
-    }
-
-    return (unsigned int)-1;
-}
-
-void init_vector(vector_t* vec){
-    vec->size = 0;
-    vec->capacity = 1;
-    vec->data = kmalloc(sizeof(unsigned int));
-}
-
-
-void vector_free(vector_t* vec, unsigned char ptrs){
-    if (ptrs){
-        for (unsigned int i = 0; i < vec->size;i++){
-            kfree((void*)vec->data[i]);
-        }
-    }
-    kfree((void*)vec->data);
 }
 
 void free_string_arr(string_array_t* str_arr){
