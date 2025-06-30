d8fa19b09506354643f31ef379a564973fa4371d
InvestedBrick <julianbrecht25@gmail.com>
2025-06-30 18:50:21 +0200

    PIT header

---
+++ src/drivers/timer/pit.h
@@ -0,0 +1,24 @@
+
+#ifndef INCLUDE_PIT_H
+#define INCLUDE_PIT_H
+
+
+#define PIT_CHANNEL0 0x40
+#define PIT_COMMAND 0x43
+/**
+ * Channel 0
+ * lobyte/hibyte
+ * square wave gen
+ * 16 bit binary mode
+ */
+#define PIT_SETTINGS 0x36 
+#define PIT_FREQUENCY 1193182
+
+/**
+ * init_pit:
+ * Initializes the Programmable Interval Timer
+ * @param desired_freq The desired frequency
+ */
+void init_pit(unsigned int desired_freq);
+
+#endif
