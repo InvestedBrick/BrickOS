a4bb0c94faf6c8db9b18e49e7c06147b1a739d50
InvestedBrick <julianbrecht25@gmail.com>
2025-07-08 21:26:35 +0200

    Correctly setting kernel stacks now

--- src/processes/scheduler.c
+++ src/processes/scheduler.c
@@ -3,11 +3,12 @@
 #include "../util.h"
 #include "../memory/memory.h"
 #include "../io/log.h"
-
+#include "user_process.h"
+#include "../tables/tss.h"
 process_state_t* p_queue;
 process_state_t* current_proc;
 unsigned char locked = 0;
-
+extern unsigned int stack_top;
 extern void kernel_process_init();
 
 void init_scheduler(){
@@ -35,21 +36,22 @@ void setup_kernel_process(interrupt_stack_frame_t* regs){
     p_queue = (process_state_t*)kmalloc(sizeof(process_state_t));
     p_queue->pd = mem_get_current_page_dir();
     memcpy(&p_queue->regs,regs,sizeof(interrupt_stack_frame_t));
+    p_queue->kernel_stack_top = stack_top;
     p_queue->next = 0;
     current_proc = p_queue;
     locked = 1;
     log("Set up kernel process");
 }
 
-void add_process_state(unsigned int* page_dir){
+void add_process_state(user_process_t* usr_proc){
     if (!p_queue) {
         error("Process queue not initialized");
         return;
     }
     process_state_t* proc = (process_state_t*)kmalloc(sizeof(process_state_t));
-    proc->pd = page_dir;
+    proc->pd = usr_proc->page_dir;
     proc->next = 0;
-
+    proc->kernel_stack_top = usr_proc->kernel_stack + MEMORY_PAGE_SIZE;
     process_state_t* last = p_queue;
 
     while(last->next) {last = last->next;}
@@ -72,6 +74,10 @@ void switch_task(interrupt_stack_frame_t* regs){
 
     if (current_proc->pd != old_pd){
         int x = breakpoint(3);
+    }
+
+    if (current_proc != p_queue){
+        set_kernel_stack(current_proc->kernel_stack_top);
     }
 
     mem_change_page_dir(current_proc->pd);
