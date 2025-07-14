3009ad897e7c959cd8ed7a097df992898021f53e
InvestedBrick <julianbrecht25@gmail.com>
2025-07-14 19:02:30 +0200

    The start of a syscall implementation

---
+++ iso/modules/cstdlib/syscalls.c
@@ -0,0 +1,20 @@
+#include "syscalls.h"
+#include "../../../src/tables/syscall_numbers.h"
+
+void interrupt(){
+    asm volatile ("int 0x30");
+}
+
+void write(unsigned int fd, const char* buffer, unsigned int count){
+    asm volatile (
+        "movl %0, %%eax\n\t"  // syscall number for write
+        "movl %1, %%ebx\n\t"  // file descriptor
+        "movl %2, %%ecx\n\t"  // buffer pointer
+        "movl %3, %%edx\n\t"  // count
+        : // no output
+        : "r"(SYS_WRITE), "r"(fd), "r"(buffer), "r"(count)
+        : "%eax", "%ebx", "%ecx", "%edx"
+    );
+
+    interrupt();
+}
