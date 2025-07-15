bf2dd414b1437880ed95e363fdd20302e73e9295
InvestedBrick <julianbrecht25@gmail.com>
2025-07-15 19:17:04 +0200

    EVERYTHING is a file

---
+++ src/filesystem/vfs/vfs.h
@@ -0,0 +1,19 @@
+
+#ifndef INCLUDE_VFS_H
+#define INCLUDE_VFS_H
+
+typedef struct generic_file_t generic_file_t;
+
+typedef struct
+{
+    generic_file_t* (*open)(unsigned char* filepath, unsigned char flags);
+    int (*read)(generic_file_t* file,unsigned char* buffer, unsigned int size);  
+    int (*write)(generic_file_t* file,unsigned char* buffer, unsigned int size);
+    int (*close)(generic_file_t* file); 
+} vfs_handlers_t;
+
+typedef struct {
+    vfs_handlers_t* ops;
+    void* generic_data; // file specific data
+} generic_file_t;
+#endif
