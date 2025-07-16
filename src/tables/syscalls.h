c32e2b8bacedefe98c775c251d944c5b509e72dc
InvestedBrick <julianbrecht25@gmail.com>
2025-07-16 21:06:23 +0200

    Generic syscall headers

---
+++ src/tables/syscalls.h
@@ -0,0 +1,23 @@
+
+#ifndef INCLUDE_SYSCALL_NUMBERS_H
+#define INCLUDE_SYSCALL_NUMBERS_H
+
+#include "../processes/user_process.h"
+
+#define SYS_READ 0x2
+#define SYS_WRITE 0x3
+#define SYS_OPEN 0x4
+#define SYS_CLOSE 0x5
+
+#define SYSCALL_FAIL -1
+
+
+int sys_write(user_process_t* p,unsigned int fd, unsigned char* buf, unsigned int size);
+
+int sys_read(user_process_t* p,unsigned int fd, unsigned char* buf, unsigned int size);
+
+int sys_open(user_process_t* p,unsigned char* filepath, unsigned char flags);
+
+int sys_close(user_process_t* p, unsigned int fd);
+
+#endif
