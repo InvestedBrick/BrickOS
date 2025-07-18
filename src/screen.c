ab349b157930883ee5ab6693eef48fb10cb79145
InvestedBrick <julianbrecht25@gmail.com>
2025-07-18 14:03:44 +0200

    Reorganised and integrated into vfs file-like status

--- src/screen.c
+++ src/screen.c
@@ -8,18 +8,6 @@ unsigned char cursor_prev_idx;
 
 char* const g_fb = (char*)VIDEO_MEMORY_START;
 
-#define CURSOR_MAX ((SCREEN_ROWS * SCREEN_COLUMNS) - 1)
-
-vfs_handlers_t screen_ops = {
-
-    //TODO: Implement
-};
-
-generic_file_t screen_file = {
-    .ops = &screen_ops,
-    .generic_data = 0
-};
-
 void clamp_cursor() {
     if (g_cursor_pos > CURSOR_MAX) g_cursor_pos = CURSOR_MAX;
     // g_cursor_pos is unsigned, so no need to check < 0
@@ -61,18 +49,6 @@ void scroll_screen_up(){
     fb_set_cursor(g_cursor_pos);
 }
 
-void write_string(const char* str, const unsigned int len){
-    for(unsigned int i = 0; i < len;i++){
-        if (g_cursor_pos >= SCREEN_ROWS * SCREEN_COLUMNS){
-            scroll_screen_up();
-        }
-        fb_write_cell(g_cursor_pos,str[i],COLOR_LIGHT_GREY,COLOR_BLACK);
-        g_cursor_pos++;
-        clamp_cursor();
-    }
-    fb_set_cursor(g_cursor_pos);
-}
-
 void erase_one_char(){
     fb_write_cell(g_cursor_pos,' ',COLOR_BLACK,COLOR_BLACK);//remove current cell
     if (g_cursor_pos != 0){
@@ -91,20 +67,25 @@ void newline(){
     }
     fb_set_cursor(g_cursor_pos);
 }
-void disable_cursor(){
-	outb(FB_COMMAND_PORT, FB_DISABLE_CURSOR_COMMAND);
-	outb(FB_DATA_PORT, FB_DISABLE_CURSOR_DATA);
+
+void write_char(unsigned char c){
+    if (g_cursor_pos >= SCREEN_ROWS * SCREEN_COLUMNS){scroll_screen_up();}
+    fb_write_cell(g_cursor_pos,c,COLOR_LIGHT_GREY,COLOR_BLACK);
+    g_cursor_pos++;
+    clamp_cursor();
+    fb_set_cursor(g_cursor_pos);
 }
-void handle_screen_input(){
-    char c;
-    if(kb_buffer_pop(&c)){
-        switch (c)
+
+void handle_screen_char_input(unsigned char c){
+    switch (c)
         {
-        case '\n':
-            {
-                
-                newline();
-            break;}
+        case '\n':{
+            newline();
+            break;
+        }
+        case '\c': { // special character for screen clearing
+            clear_screen();
+        }
         case '\t':{
             if (g_cursor_pos < SCREEN_ROWS * SCREEN_COLUMNS - 4){
                 g_cursor_pos += 4;
@@ -120,11 +101,53 @@ void handle_screen_input(){
             break;
         }
         default:{
-                write_string(&c,1);
-                break;
-            }
+            write_char(c);
+            break;
         }
     }
+    
     clamp_cursor();
     fb_set_cursor(g_cursor_pos);
+}
+
+int screen_read(generic_file_t* f, unsigned char* buffer, unsigned int size){
+    if (size > SCREEN_PIXELS)
+        return INVALID_ARGUMENT;
+
+    for (unsigned int i = 0; i < size; i++){
+        buffer[i] = g_fb[i * 2];
+    }
+
+    return size;
+}
+
+int screen_write(generic_file_t* f, unsigned char* buffer,unsigned int size){
+    if (size > SCREEN_PIXELS) 
+        return INVALID_ARGUMENT;
+    
+    for(unsigned int i = 0; i < size;i++){
+        handle_screen_char_input(buffer[i]);
+    }
+
+    return size;
+        
+}
+
+vfs_handlers_t screen_ops = {
+
+    .close = 0,
+    .open = 0,
+    .read = screen_read,
+    .write = screen_write,
+};
+
+generic_file_t screen_file = {
+    .ops = &screen_ops,
+    .generic_data = 0
+};
+
+
+void disable_cursor(){
+	outb(FB_COMMAND_PORT, FB_DISABLE_CURSOR_COMMAND);
+	outb(FB_DATA_PORT, FB_DISABLE_CURSOR_DATA);
 }
