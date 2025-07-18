de59087595496d09093ae60bdee10a1abc71733e
InvestedBrick <julianbrecht25@gmail.com>
2025-07-18 16:07:25 +0200

    Fixed std fds

--- iso/modules/cstdlib/syscalls.h
+++ iso/modules/cstdlib/syscalls.h
@@ -2,8 +2,9 @@
 #ifndef INCLUDE_SYSCALLS_H
 #define INCLUDE_SYSCALLS_H
 
-#define FD_STDOUT 0x0
-#define FD_STDIN 0x1
+#define FD_STDIN 0x0
+#define FD_STDOUT 0x1
+#define FD_STDERR 0x2
 
 void write(unsigned int fd, const char* buffer, unsigned int count);
 
