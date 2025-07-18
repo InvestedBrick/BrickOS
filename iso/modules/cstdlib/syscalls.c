cc9b981594b09d7a0b6339f03d1cddb65989de9d
InvestedBrick <julianbrecht25@gmail.com>
2025-07-18 16:06:50 +0200

    Fixed inline asm

--- iso/modules/cstdlib/syscalls.c
+++ iso/modules/cstdlib/syscalls.c
@@ -1,20 +1,11 @@
 #include "syscalls.h"
 #include "../../../src/tables/syscalls.h"
 
-void interrupt(){
-    asm volatile ("int 0x30");
-}
-
 void write(unsigned int fd, const char* buffer, unsigned int count){
     asm volatile (
-        "movl %0, %%eax\n\t"  // syscall number for write
-        "movl %1, %%ebx\n\t"  // file descriptor
-        "movl %2, %%ecx\n\t"  // buffer pointer
-        "movl %3, %%edx\n\t"  // count
-        : // no output
-        : "r"(SYS_WRITE), "r"(fd), "r"(buffer), "r"(count)
-        : "%eax", "%ebx", "%ecx", "%edx"
+        "int $0x30"
+        :
+        : "a"(SYS_WRITE), "b"(fd), "c"(buffer), "d"(count)
+        : "memory"
     );
-
-    interrupt();
 }
