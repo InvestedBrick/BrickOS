7dbdd535e717ca329ffe7b92ea4522ee3a2e5c57
InvestedBrick <julianbrecht25@gmail.com>
2025-07-22 18:56:39 +0200

    Added other syscalls

--- iso/modules/cstdlib/syscalls.c
+++ iso/modules/cstdlib/syscalls.c
@@ -1,11 +1,38 @@
 #include "syscalls.h"
 #include "../../../src/tables/syscalls.h"
 
-void write(unsigned int fd, const char* buffer, unsigned int count){
+int write(unsigned int fd, const char* buffer, unsigned int count){
     asm volatile (
         "int $0x30"
         :
         : "a"(SYS_WRITE), "b"(fd), "c"(buffer), "d"(count)
+        : "memory"
+    );
+}
+
+int read(unsigned int fd, const char* buffer, unsigned int count){
+    asm volatile (
+        "int $0x30"
+        :
+        : "a"(SYS_READ), "b"(fd), "c"(buffer), "d"(count)
+        : "memory"
+    );
+}
+
+int open(const char* pathname, unsigned char flags){
+    asm volatile (
+        "int $0x30"
+        :
+        : "a"(SYS_OPEN), "b"(pathname), "c"(flags)
+        : "memory"
+    );
+}
+
+int close(unsigned int fd){
+    asm volatile (
+        "int $0x30"
+        :
+        : "a"(SYS_CLOSE), "b"(fd)
         : "memory"
     );
 }
