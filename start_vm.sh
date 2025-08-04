ccf0d8b82c39dfe5c34068aa6923039dadb90ad6
InvestedBrick <julianbrecht25@gmail.com>
2025-08-04 19:01:24 +0200

    Added arg comment

--- start_vm.sh
+++ start_vm.sh
@@ -1,3 +1,5 @@
 qemu-system-i386 -s -d int -drive file=hdd.img,format=raw,if=ide -serial file:serial.log -boot d -cdrom BrickOS.iso -m 512 
 
 # add "-d int" as argument for more debug info 
+# -s for gdb remote target
+# -S to stop execution at start
