eb19118780888f40ae63ffb02fdbc0a6ab47a241
InvestedBrick <julianbrecht25@gmail.com>
2025-08-04 18:58:30 +0200

    renamed

---
+++ src/kernel_header.h
@@ -0,0 +1,10 @@
+
+#ifndef INCLUDE_KERNEL_PROCESS_H
+#define INCLUDE_KERNEL_PROCESS_H
+#include "processes/user_process.h"
+
+extern user_process_t global_kernel_process;
+
+void shutdown();
+
+#endif
