5550a63962650ba4dc248e32b7b4725e46815e4b
InvestedBrick <julianbrecht25@gmail.com>
2025-08-07 18:47:58 +0200

    The probably most inefficient input reader

--- iso/modules/cstdlib/stdio.c
+++ iso/modules/cstdlib/stdio.c
@@ -7,3 +7,21 @@ void print(const char* str){
 
     write(FD_STDOUT,str,str_len);
 }
+
+int read_input(char* buffer,unsigned int buffer_size){
+    int read_chars = 0;
+    char c;
+    while(1){
+        if (read(FD_STDIN,&c,1) == 1){
+            if (read_chars >= buffer_size || c == '\n')
+                break;
+
+            buffer[read_chars++] = c;
+
+            write(FD_STDOUT,&c,1);
+        }
+    }
+
+    return read_chars;
+    
+}
