6fa0268918e428c39545cda077f8347a9e5ce7d8
InvestedBrick <julianbrecht25@gmail.com>
2025-08-08 13:00:22 +0200

    Wrong syscalls.h

--- iso/modules/cstdlib/syscalls.h
+++ iso/modules/cstdlib/syscalls.h
@@ -2,9 +2,6 @@
 #ifndef INCLUDE_SYSCALLS_H
 #define INCLUDE_SYSCALLS_H
 
-#include "interrupts.h"
-#include "user_process.h"
-
 #define FD_STDIN 0x0
 #define FD_STDOUT 0x1
 #define FD_STDERR 0x2
@@ -15,5 +12,5 @@ int read(unsigned int fd, const char* buffer, unsigned int count);
 int open(const char* pathname, unsigned char flags);
 int close(unsigned int fd);
 int exit(unsigned int error_code);
-int sys_exit(user_process_t* p,interrupt_stack_frame_t* stack_frame);
+
 #endif
