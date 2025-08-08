b34760b4fcd75361ffdd9b0818a515fc75dbd790
InvestedBrick <julianbrecht25@gmail.com>
2025-08-08 16:16:29 +0200

    Allocating dynamic memory!

--- iso/modules/c_test.c
+++ iso/modules/c_test.c
@@ -4,8 +4,8 @@
 void main(){
     print("\nHello from module\n");
     print("What is your name?: ");
-    char input_buffer[33];
-    int bytes_read = read_input(input_buffer,32);
+    char* input_buffer = (char*)alloc_page();
+    int bytes_read = read_input(input_buffer,80);
     input_buffer[bytes_read] = 0;
     print("\nHello ");
     print(input_buffer);
