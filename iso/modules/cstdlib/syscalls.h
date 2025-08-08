3160b86f9d7d61ad4ce7b66ab79c71be29861506
InvestedBrick <julianbrecht25@gmail.com>
2025-08-08 16:17:35 +0200

    Function documentation and alloc_page header

--- iso/modules/cstdlib/syscalls.h
+++ iso/modules/cstdlib/syscalls.h
@@ -1,4 +1,3 @@
-
 #ifndef INCLUDE_SYSCALLS_H
 #define INCLUDE_SYSCALLS_H
 
@@ -6,11 +5,61 @@
 #define FD_STDOUT 0x1
 #define FD_STDERR 0x2
 
+/**
+ * write:
+ * Writes a buffer to a file
+ * @param fd The file descriptor to the file
+ * @param buffer The buffer to write
+ * @param count The length of the buffer
+ * 
+ * @return The number of bytes written
+ */
 int write(unsigned int fd, const char* buffer, unsigned int count);
+
+/**
+ * read:
+ * Reads data from a file into a buffer
+ * @param fd The file descriptor to the file
+ * @param buffer The buffer to read into
+ * @param count The maximum number of bytes to read
+ * 
+ * @return The number of bytes read
+ */
 int read(unsigned int fd, const char* buffer, unsigned int count);
 
+/**
+ * open:
+ * Opens a file
+ * @param pathname The path to the file
+ * @param flags Flags for opening the file
+ * 
+ * @return The file descriptor for the opened file
+ */
 int open(const char* pathname, unsigned char flags);
+
+/**
+ * close:
+ * Closes a file descriptor
+ * @param fd The file descriptor to close
+ * 
+ * @return 0 on success, or -1 on error
+ */
 int close(unsigned int fd);
+
+/**
+ * exit:
+ * Exits the current process
+ * @param error_code The exit code
+ * 
+ * @return Does not return
+ */
 int exit(unsigned int error_code);
 
+/**
+ * alloc_page:
+ * Allocates a memory page (4kb)
+ * 
+ * @return Pointer to the allocated page
+ */
+void* alloc_page();
 #endif
