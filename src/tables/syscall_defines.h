e30720284d91355d8633b57bde24452c73724129
InvestedBrick <julianbrecht25@gmail.com>
2025-08-08 16:23:06 +0200

    Moved to extra file so that all of the syscalls are not included in the cstdlib of my user programs

---
+++ src/tables/syscall_defines.h
@@ -0,0 +1,14 @@
+
+#ifndef INCLUDE_SYSCALL_DEFINES_H
+#define INCLUDE_SYSCALL_DEFINES_H
+
+#define SYS_EXIT 0x1
+#define SYS_READ 0x2
+#define SYS_WRITE 0x3
+#define SYS_OPEN 0x4
+#define SYS_CLOSE 0x5
+#define SYS_ALLOC_PAGE 0x6
+
+#define SYSCALL_FAIL -1
+
+#endif
