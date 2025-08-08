72de9f6d366d086ba4b6f6e9f80e0c2877017afa
InvestedBrick <julianbrecht25@gmail.com>
2025-08-08 16:22:34 +0200

    alloc page header

--- src/tables/syscalls.h
+++ src/tables/syscalls.h
@@ -3,16 +3,7 @@
 #define INCLUDE_SYSCALL_NUMBERS_H
 
 #include "../processes/user_process.h"
-
-#define SYS_READ 0x2
-#define SYS_WRITE 0x3
-#define SYS_OPEN 0x4
-#define SYS_CLOSE 0x5
-#define SYS_EXIT 0x1
-
-#define SYSCALL_FAIL -1
-
-
+#include "interrupts.h"
 int sys_write(user_process_t* p,unsigned int fd, unsigned char* buf, unsigned int size);
 
 int sys_read(user_process_t* p,unsigned int fd, unsigned char* buf, unsigned int size);
@@ -23,4 +14,5 @@ int sys_close(user_process_t* p, unsigned int fd);
 
 int sys_exit(user_process_t* p,interrupt_stack_frame_t* stack_frame);
 
+int sys_alloc_page(user_process_t* p,interrupt_stack_frame_t* stack_frame);
 #endif
