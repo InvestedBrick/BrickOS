97982fba58c2669aeab928516273f86dd7ad194a
InvestedBrick <julianbrecht25@gmail.com>
2025-08-09 15:00:18 +0200

    Using malloc now

--- iso/modules/c_test.c
+++ iso/modules/c_test.c
@@ -1,10 +1,11 @@
 #include "cstdlib/stdio.h"
 #include "cstdlib/stdutils.h"
 #include "cstdlib/syscalls.h"
+#include "cstdlib/malloc.h"
 void main(){
     print("\nHello from module\n");
     print("What is your name?: ");
-    char* input_buffer = (char*)alloc_page();
+    char* input_buffer = (char*)malloc(100);
     int bytes_read = read_input(input_buffer,80);
     input_buffer[bytes_read] = 0;
     print("\nHello ");
