211ea9b6dc8cbad2c8136f6e421875f4f1d1fdf9
InvestedBrick <julianbrecht25@gmail.com>
2025-08-11 21:01:08 +0200

    Added SCREEN_ROWS and COLUMNS

--- iso/modules/cstdlib/stdio.h
+++ iso/modules/cstdlib/stdio.h
@@ -2,6 +2,9 @@
 #ifndef INCLUDE_STDIO_H
 #define INCLUDE_STDIO_H
 
+#define SCREEN_ROWS 25
+#define SCREEN_COLUMNS 80
+
 void print(const char* str);
 
 int read_input(char* buffer,unsigned int buffer_size);
