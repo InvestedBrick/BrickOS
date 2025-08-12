14ad97f0f19568b0e600b43b59b50278053ac20d
InvestedBrick <julianbrecht25@gmail.com>
2025-08-12 20:28:34 +0200

    now it kinda looks like a shell

--- iso/modules/shell.c
+++ iso/modules/shell.c
@@ -3,18 +3,34 @@
 #include "cstdlib/syscalls.h"
 #include "cstdlib/malloc.h"
 void main(){
-    print("\nHello from module\n");
+    print("\nBrick OS Shell - type 'help' for command list\n");
     unsigned char* line = (unsigned char*)malloc(SCREEN_COLUMNS + 1);
     unsigned char* dir_buffer = (unsigned char*)malloc(100);
     while(1){
         memset(line,0x0,SCREEN_COLUMNS + 1);
-        print("\n|-(");
+        print("|-(");
         getcwd(dir_buffer,100);
         print(dir_buffer);
         print(")-> ");
         int read_bytes = read_input(line,SCREEN_COLUMNS);
         line[read_bytes] = '\0';
-        exit(1);
+        print("\n");
+        if (streq(line,"help")){
+            print("Shell command list\n------------------\n");
+            print("clear - Clears the screen\n");
+            print("exit - Exits the shell and shuts down the OS\n\n");
+
+        }
+        else if (streq(line,"exit")){
+            break;
+        }
+        else if (streq(line,"clear")){
+            print("\e"); // special clear char
+        }else{
+            print("Command '");
+            print(line);
+            print("' was not found!\n");
+        }
     }
 
     exit(1);
