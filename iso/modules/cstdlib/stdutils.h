1e678dcb0c7a9061c9a9bf8333718a6fcb6c880c
InvestedBrick <julianbrecht25@gmail.com>
2025-08-12 20:28:15 +0200

    streq header

--- iso/modules/cstdlib/stdutils.h
+++ iso/modules/cstdlib/stdutils.h
@@ -5,7 +5,10 @@
 #define CEIL_DIV(a,b) (((a + b) - 1 )/ b)
 
 unsigned int strlen(const char* str);
+
 void* memset(void* dest, int val, unsigned int n);
+
 void loop();
 
+unsigned char streq(const char* str1, const char* str2);
 #endif
