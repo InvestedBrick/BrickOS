b81767fbd29acda0f82ef424107477d906e2441e
InvestedBrick <julianbrecht25@gmail.com>
2025-08-13 12:52:52 +0200

    getdents header

--- src/tables/syscalls.h
+++ src/tables/syscalls.h
@@ -4,6 +4,9 @@
 
 #include "../processes/user_process.h"
 #include "interrupts.h"
+
+#include "../filesystem/filesystem.h"
+
 int sys_write(user_process_t* p,unsigned int fd, unsigned char* buf, unsigned int size);
 
 int sys_read(user_process_t* p,unsigned int fd, unsigned char* buf, unsigned int size);
@@ -17,4 +20,6 @@ int sys_exit(user_process_t* p,interrupt_stack_frame_t* stack_frame);
 int sys_mmap(user_process_t* p,unsigned int size);
 
 int sys_getcwd(unsigned char* buffer, unsigned int buf_len);
+
+int sys_getdents(user_process_t* p,unsigned int fd,dirent_t* ent_buffer,unsigned int size);
 #endif
