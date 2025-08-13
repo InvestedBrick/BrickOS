d001296185e15c05c3117680ddf79dd54a0d306c
InvestedBrick <julianbrecht25@gmail.com>
2025-08-13 12:46:16 +0200

    Working ls implementation!!

--- iso/modules/shell.c
+++ iso/modules/shell.c
@@ -3,7 +3,7 @@
 #include "cstdlib/syscalls.h"
 #include "cstdlib/malloc.h"
 void main(){
-    print("\nBrick OS Shell - type 'help' for command list\n");
+    print("\nBrickOS Shell started\n");
     unsigned char* line = (unsigned char*)malloc(SCREEN_COLUMNS + 1);
     unsigned char* dir_buffer = (unsigned char*)malloc(100);
     while(1){
@@ -18,6 +18,7 @@ void main(){
         if (streq(line,"help")){
             print("Shell command list\n------------------\n");
             print("clear - Clears the screen\n");
+            print("ls - Lists all entries of the current directory\n");
             print("exit - Exits the shell and shuts down the OS\n\n");
 
         }
@@ -26,7 +27,28 @@ void main(){
         }
         else if (streq(line,"clear")){
             print("\e"); // special clear char
-        }else{
+        }
+        else if (streq(line,"ls")){
+            unsigned int dir_fd = open(dir_buffer,FILE_FLAG_NONE);
+
+            unsigned char buffer[1024];
+
+            int n_entries = getdents(dir_fd,(dirent_t*)buffer,sizeof(buffer));
+
+            if (n_entries == -1) {print("Unable to read directory entries\n");continue;};
+
+            unsigned int bpos = 0;
+            for (unsigned int i = 0; i < n_entries;i++){
+                dirent_t* ent = (dirent_t*)(buffer + bpos);
+                print(ent->name);
+                if (ent->type == TYPE_DIR){
+                    print("/");
+                }
+                print("\n");
+                bpos += ent->len;
+            }
+        }
+        else{
             print("Command '");
             print(line);
             print("' was not found!\n");
