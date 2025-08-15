aa1f8dcecaf1b0db4b927e9aa44c7c5485a02c2a
InvestedBrick <julianbrecht25@gmail.com>
2025-08-15 13:59:10 +0200

    Including entire fs defines now

--- iso/modules/cstdlib/fs.h
+++ iso/modules/cstdlib/fs.h
@@ -2,13 +2,7 @@
 #ifndef INCLUDE_FS_H
 #define INCLUDE_FS_H
 
-#define FILE_FLAG_NONE 0x0
-#define FILE_FLAG_READ 0x1
-#define FILE_FLAG_WRITE 0x2
-#define FILE_PERM_EXEC 0x4
-#define FILE_FLAG_CREATE 0x4
-#define TYPE_FILE 1
-#define TYPE_DIR 2
+#include "../../../src/filesystem/fs_defines.h"
 
 typedef struct {
     unsigned int inode_id;
