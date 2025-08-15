b94b5672b3f316dc57eea540160eab54c19005b1
InvestedBrick <julianbrecht25@gmail.com>
2025-08-15 13:02:06 +0200

    File flag create define

--- iso/modules/cstdlib/fs.h
+++ iso/modules/cstdlib/fs.h
@@ -5,8 +5,8 @@
 #define FILE_FLAG_NONE 0x0
 #define FILE_FLAG_READ 0x1
 #define FILE_FLAG_WRITE 0x2
-#define FILE_FLAG_EXEC 0x4
-
+#define FILE_PERM_EXEC 0x4
+#define FILE_FLAG_CREATE 0x4
 #define TYPE_FILE 1
 #define TYPE_DIR 2
 
