bd19327056aca087954286ed125bf10e292a78f3
InvestedBrick <julianbrecht25@gmail.com>
2025-08-17 16:12:57 +0200

    Added printing uints

--- iso/modules/cstdlib/stdio.c
+++ iso/modules/cstdlib/stdio.c
@@ -7,7 +7,22 @@ void print(const char* str){
 
     write(FD_STDOUT,str,str_len);
 }
-
+void print_uint(unsigned int num){
+    //convert num to ASCII
+    char ascii[11] = {0};
+    if(num == 0){
+        print("0");
+        return;
+    }
+    unsigned int temp,count = 0,i;
+    for(temp=num;temp != 0; temp /= 10,count++);
+    for (i = count - 1, temp=num;i < 0xffffffff;i--){
+        ascii[i] = (temp%10) + 0x30;
+        temp /= 10;
+    }
+    
+    print(ascii);
+}
 int read_input(char* buffer,unsigned int buffer_size){
     int read_chars = 0;
     char c;
