3444edc628886bac5c97f7f87fbaa0f3f7d3f24e
InvestedBrick <julianbrecht25@gmail.com>
2025-08-21 16:13:13 +0200

    using grub2

--- stage3.sh
+++ stage3.sh
@@ -1,10 +1,1 @@
-genisoimage -R                                  \
-            -b boot/grub/stage2_eltorito        \
-            -no-emul-boot                       \
-            -boot-load-size 4                   \
-            -A BrickOS                          \
-            -input-charset utf8                 \
-            -quiet                              \
-            -boot-info-table                    \
-            -o BrickOS.iso                      \
-            iso
+grub-mkrescue -o BrickOS.iso iso --modules="vbe vga video gfxterm"
