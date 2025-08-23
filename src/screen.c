4e771852032743d82bd1366d6d745e083d732e3d
InvestedBrick <julianbrecht25@gmail.com>
2025-08-23 10:11:29 +0200

    fixed rgb to color

--- src/screen.c
+++ src/screen.c
@@ -1,49 +1,82 @@
 #include "screen.h"
 #include "io/io.h"
+#include "io/log.h"
 #include "drivers/keyboard/keyboard.h"
 #include "filesystem/vfs/vfs.h"
 #include "util.h"
 #include "memory/memory.h"
 #include "multiboot.h"
+#include <stdint.h>
+char* g_fb = (char*)VIDEO_MEMORY_START;
+uint16_t g_cursor_pos = 0;
+uint8_t cursor_prev_idx;
 
-char* const g_fb = (char*)VIDEO_MEMORY_START;
-unsigned short g_cursor_pos = 0;
-unsigned char cursor_prev_idx;
+volatile uint8_t* fb = 0;
+uint32_t screen_width;
+uint32_t screen_height;
+uint32_t screen_bytes_per_row;
+uint8_t screen_bytespp;
 
-volatile unsigned char* fb = 0;
-unsigned int screen_width;
-unsigned int screen_height;
-unsigned int screen_bytes_per_row;
-unsigned char screen_bytespp;
+uint8_t r_pos = 0;
+uint8_t r_mask_size = 0;
+
+uint8_t g_pos = 0;
+uint8_t g_mask_size = 0;
+
+uint8_t b_pos = 0;
+uint8_t b_mask_size = 0;
 
 // preparation for multiple screens
-unsigned int screen_origin_x = 0;
-unsigned int screen_origin_y = 0;
-void map_framebuffer(unsigned int fb_phys,unsigned int fb_virt, unsigned int fb_size){
-    unsigned int n_pages = CEIL_DIV(fb_size,MEMORY_PAGE_SIZE);
-    for (unsigned int i = 0; i < n_pages;i++){
-        mem_map_page(fb_virt + i * MEMORY_PAGE_SIZE, fb_phys + i * MEMORY_PAGE_SIZE, PAGE_FLAG_WRITE | PAGE_FLAG_OWNER);
+uint32_t screen_origin_x = 0;
+uint32_t screen_origin_y = 0;
+
+uint32_t rgb_to_color(uint8_t r, uint8_t g, uint8_t b) {
+    // assuming 32bit ARGB
+    uint32_t color = 0;
+
+    color |= ((uint32_t)r << 16);
+    color |= ((uint32_t)g << 8);
+    color |= ((uint32_t)b << 0);
+
+    // optional: force alpha to opaque if your format uses it
+    color |= 0xff000000;
+
+    return color;
+}
+
+void map_framebuffer(uint32_t fb_phys,uint32_t fb_virt, uint32_t fb_size){
+
+    uint32_t n_pages = CEIL_DIV(fb_size,MEMORY_PAGE_SIZE);
+    for (uint32_t i = 0; i < n_pages;i++){
+        mem_map_page(fb_virt + i * MEMORY_PAGE_SIZE,fb_phys + i * MEMORY_PAGE_SIZE, PAGE_FLAG_WRITE | PAGE_FLAG_OWNER);
     } 
 }
 
-void init_framebuffer(multiboot_info_t* mboot,unsigned int fb_start){
+void init_framebuffer(multiboot_info_t* mboot,uint32_t fb_start){
 
-    unsigned int fb_phys = (unsigned int)mboot->framebuffer_addr;
-    unsigned int fb_size = mboot->framebuffer_pitch * mboot->framebuffer_height;
+    uint32_t fb_phys = (uint32_t)mboot->framebuffer_addr;
+    uint32_t fb_size = mboot->framebuffer_pitch * mboot->framebuffer_height;
 
-    fb = (volatile unsigned char*)(fb_start);
+    fb = (volatile uint8_t*)(fb_start);
     screen_width             = mboot->framebuffer_width;
     screen_height            = mboot->framebuffer_height;
     screen_bytes_per_row     = mboot->framebuffer_pitch;
     screen_bytespp           = mboot->framebuffer_bpp / 8;
+    r_pos                    = mboot->framebuffer_red_field_pos;
+    r_mask_size              = mboot->framebuffer_red_mask_size;
+    g_pos                    = mboot->framebuffer_green_field_pos;
+    g_mask_size              = mboot->framebuffer_green_mask_size;
+    b_pos                    = mboot->framebuffer_blue_field_pos;
+    b_mask_size              = mboot->framebuffer_blue_mask_size;
+
     map_framebuffer(fb_phys,fb_start,fb_size);
 }
 
-void write_pixel(unsigned int x, unsigned int y, unsigned int color){
+void write_pixel(uint32_t x, uint32_t y, uint32_t color){
     if (x >= screen_width || x < screen_origin_x) return;
     if (y >= screen_height || y < screen_origin_y) return;
 
-    *(volatile unsigned int*)(fb + y * screen_bytes_per_row + x * screen_bytespp) = color;
+    *(volatile uint32_t*)(fb + y * screen_bytes_per_row + x * screen_bytespp) = color;
 }
 
 void clamp_cursor() {
@@ -51,13 +84,13 @@ void clamp_cursor() {
     // g_cursor_pos is unsigned, so no need to check < 0
 }
 
-void fb_write_cell(unsigned short i, char c,unsigned char fg, unsigned char bg){
+void fb_write_cell(uint16_t i, char c,uint8_t fg, uint8_t bg){
     g_fb[i * 2] = c;
     g_fb[i * 2 + 1] = ((bg & 0x0f) << 4 | (fg & 0x0f));
 }
 
 void clear_screen(){
-    unsigned short i = 0;
+    uint16_t i = 0;
     while (i <  SCREEN_ROWS * SCREEN_COLUMNS){
         fb_write_cell(i,' ',COLOR_BLACK,COLOR_BLACK);
         i++;
@@ -65,18 +98,18 @@ void clear_screen(){
     g_cursor_pos = 0;
 }
 
-void fb_set_cursor(unsigned short pos) {
+void fb_set_cursor(uint16_t pos) {
     clamp_cursor();
     fb_write_cell(pos,' ',COLOR_LIGHT_GREY,COLOR_LIGHT_GREY);
 }
 
 void scroll_screen_up(){
-    for (unsigned short idx = 0; idx < (SCREEN_ROWS * SCREEN_COLUMNS) - SCREEN_COLUMNS;idx++){
+    for (uint16_t idx = 0; idx < (SCREEN_ROWS * SCREEN_COLUMNS) - SCREEN_COLUMNS;idx++){
         g_fb[idx * 2] = g_fb[(idx + SCREEN_COLUMNS) * 2];
         g_fb[idx * 2 + 1] = g_fb[(idx + SCREEN_COLUMNS) * 2 + 1];
     }
 
-    for(unsigned short idx = (SCREEN_ROWS * SCREEN_COLUMNS) - SCREEN_COLUMNS; idx <  (SCREEN_ROWS * SCREEN_COLUMNS);idx++){
+    for(uint16_t idx = (SCREEN_ROWS * SCREEN_COLUMNS) - SCREEN_COLUMNS; idx <  (SCREEN_ROWS * SCREEN_COLUMNS);idx++){
         fb_write_cell(idx,' ',COLOR_BLACK,COLOR_BLACK);
     }
     if (g_cursor_pos >= SCREEN_COLUMNS)
@@ -106,7 +139,7 @@ void newline(){
     fb_set_cursor(g_cursor_pos);
 }
 
-void write_char(unsigned char c){
+void write_char(uint8_t c){
     if (g_cursor_pos >= SCREEN_ROWS * SCREEN_COLUMNS){scroll_screen_up();}
     fb_write_cell(g_cursor_pos,c,COLOR_LIGHT_GREY,COLOR_BLACK);
     g_cursor_pos++;
@@ -114,7 +147,7 @@ void write_char(unsigned char c){
     fb_set_cursor(g_cursor_pos);
 }
 
-void handle_screen_char_input(unsigned char c){
+void handle_screen_char_input(uint8_t c){
     switch (c)
         {
         case '\n':{
@@ -149,22 +182,22 @@ void handle_screen_char_input(unsigned char c){
     fb_set_cursor(g_cursor_pos);
 }
 
-int screen_read(generic_file_t* f, unsigned char* buffer, unsigned int size){
+int screen_read(generic_file_t* f, uint8_t* buffer, uint32_t size){
     if (size > SCREEN_PIXELS)
         return INVALID_ARGUMENT;
 
-    for (unsigned int i = 0; i < size; i++){
+    for (uint32_t i = 0; i < size; i++){
         buffer[i] = g_fb[i * 2];
     }
 
     return size;
 }
 
-int screen_write(generic_file_t* f, unsigned char* buffer,unsigned int size){
+int screen_write(generic_file_t* f, uint8_t* buffer,uint32_t size){
     if (size > SCREEN_PIXELS) 
         return INVALID_ARGUMENT;
     
-    for(unsigned int i = 0; i < size;i++){
+    for(uint32_t i = 0; i < size;i++){
         handle_screen_char_input(buffer[i]);
     }
 
