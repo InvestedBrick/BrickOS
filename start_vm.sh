11b2a75d1dfac0a507e49ceba55543c6881473f6
InvestedBrick <julianbrecht25@gmail.com>
2025-08-23 10:14:12 +0200

    Cleaned up

--- start_vm.sh
+++ start_vm.sh
@@ -1,4 +1,10 @@
-qemu-system-i386 -s -d int -drive file=hdd.img,format=raw,if=ide -serial file:serial.log -boot d -cdrom BrickOS.iso -m 512 
+qemu-system-i386 -s \
+    -drive file=hdd.img,format=raw,if=ide \
+    -serial file:serial.log \
+    -boot d \
+    -cdrom BrickOS.iso \
+    -m 512 \
+    -vga std 
 
 # add "-d int" as argument for more debug info 
 # -s for gdb remote target
