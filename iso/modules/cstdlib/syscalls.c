d4ebd7b68d99db96fccf6647240cbeee7a91c9b2
InvestedBrick <julianbrecht25@gmail.com>
2025-08-24 21:55:32 +0200

    mad return value clearer and added seek syscall

--- iso/modules/cstdlib/syscalls.c
+++ iso/modules/cstdlib/syscalls.c
@@ -1,92 +1,123 @@
 #include "syscalls.h"
 #include "fs.h"
 
-int write(uint32_t fd, const char* buffer, uint32_t count){
+int write(uint32_t fd, const char* buffer, uint32_t count) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_WRITE), "b"(fd), "c"(buffer), "d"(count)
         : "memory"
     );
+    return ret;
 }
 
-int read(uint32_t fd, const char* buffer, uint32_t count){
+int read(uint32_t fd, const char* buffer, uint32_t count) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_READ), "b"(fd), "c"(buffer), "d"(count)
         : "memory"
     );
+    return ret;
 }
 
-int open(const char* pathname, uint8_t flags){
+int open(const char* pathname, uint8_t flags) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_OPEN), "b"(pathname), "c"(flags)
         : "memory"
     );
+    return ret;
 }
 
-int close(uint32_t fd){
+int close(uint32_t fd) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_CLOSE), "b"(fd)
         : "memory"
     );
+    return ret;
 }
 
-int exit(uint32_t error_code){
+int exit(uint32_t error_code) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_EXIT), "b"(error_code)
         : "memory"
     );
+    return ret; // does not return, just convention
 }
 
-void* mmap(uint32_t size){
+void* mmap(uint32_t size, uint32_t prot, uint32_t flags,
+           uint32_t fd, uint32_t offset) {
+    void* ret;
     asm volatile (
         "int $0x30"
-        :
-        : "a"(SYS_ALLOC_PAGE), "b"(size)
+        : "=a"(ret)
+        : "a"(SYS_ALLOC_PAGE), "b"(size), "c"(prot),
+          "d"(flags), "D"(fd), "S"(offset)
         : "memory"
     );
+    return ret;
 }
 
-void getcwd(unsigned char* buffer, uint32_t size){
+void getcwd(unsigned char* buffer, uint32_t size) {
     asm volatile (
         "int $0x30"
-        :
+        : 
         : "a"(SYS_GETCWD), "b"(buffer), "c"(size)
         : "memory"
     );
 }
 
-int getdents(uint32_t fd,dirent_t* buffer,uint32_t size){
+int getdents(uint32_t fd, dirent_t* buffer, uint32_t size) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_GETDENTS), "b"(fd), "c"(buffer), "d"(size)
         : "memory"
     );
+    return ret;
 }
 
-int chdir(unsigned char* dir_name){
+int chdir(unsigned char* dir_name) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_CHDIR), "b"(dir_name)
         : "memory"
     );
+    return ret;
 }
 
-int rmfile(unsigned char* filename){
+int rmfile(unsigned char* filename) {
+    int ret;
     asm volatile (
         "int $0x30"
-        :
+        : "=a"(ret)
         : "a"(SYS_RMFILE), "b"(filename)
         : "memory"
     );
+    return ret;
+}
+
+int seek(uint32_t fd,uint32_t offset){
+    int ret;
+    asm volatile (
+        "int $0x30"
+        : "=a"(ret)
+        : "a"(SYS_SEEK), "b"(fd), "c"(offset)
+        : "memory"
+    );
+    return ret;
 }
