72f4e10a8ec1c28390de8b315519df3d86162fa6
InvestedBrick <julianbrecht25@gmail.com>
2025-08-26 19:36:24 +0200

    shared device definitions

---
+++ src/filesystem/devices/device_defines.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDE_DEVICE_COMMANDS_H
+#define INCLUDE_DEVICE_COMMANDS_H
+#include <stdint.h>
+
+#define DEV_FB0_GET_METADATA 0x01
+
+typedef struct {
+    uint32_t phys_addr;
+    uint32_t width;
+    uint32_t height;
+    uint32_t bpp;
+    uint32_t bytes_per_row;
+    uint32_t size;
+}framebuffer_t;
+
+#endif
