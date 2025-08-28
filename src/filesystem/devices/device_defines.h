8498a0a73db30aa4f3e9e7a0cdd168ffc57025ae
InvestedBrick <julianbrecht25@gmail.com>
2025-08-28 20:05:32 +0200

    window request struct

--- src/filesystem/devices/device_defines.h
+++ src/filesystem/devices/device_defines.h
@@ -5,6 +5,9 @@
 // these defines / structs are accessible for the userspace
 #define DEV_FB0_GET_METADATA 0x01
 
+#define DEV_WM_REQUEST_WINDOW 0x01
+
+#define WINDOW_REQ_FLAG_DECORATION 0x2
 typedef struct {
     uint32_t phys_addr;
     uint32_t width;
@@ -13,5 +16,11 @@ typedef struct {
     uint32_t bytes_per_row;
     uint32_t size;
 }framebuffer_t;
+
+typedef struct {
+    uint32_t width;
+    uint32_t height;
+    uint32_t flags;
+}window_req_t;
 
 #endif
