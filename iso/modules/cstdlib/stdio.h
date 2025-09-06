34a11fa26f84e5187e97622235dcc07a337d111e
InvestedBrick <julianbrecht25@gmail.com>
2025-09-06 23:31:42 +0200

    removed old definitions

--- iso/modules/cstdlib/stdio.h
+++ iso/modules/cstdlib/stdio.h
@@ -2,8 +2,6 @@
 #ifndef INCLUDE_STDIO_H
 #define INCLUDE_STDIO_H
 
-#define SCREEN_ROWS 25
-#define SCREEN_COLUMNS 80
 #include <stdint.h>
 void print(const char* str);
 void print_uint(uint32_t num);
