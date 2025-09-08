1226bde867786c2fcd07f5a2fd56d75d5f9efa19
InvestedBrick <julianbrecht25@gmail.com>
2025-09-09 01:06:58 +0200

    focused window and input sharing

--- iso/modules/win_man.c
+++ iso/modules/win_man.c
@@ -17,6 +17,7 @@ typedef struct window {
     uint32_t flags;
     uint32_t z;
     uint32_t owner_pid;
+    uint32_t kb_pipe_fd;
     int backing_fd;
     uint32_t buffer_size;
     unsigned char* live_buffer;
@@ -25,7 +26,7 @@ typedef struct window {
 }window_t;
 
 window_t* head = 0; 
-
+window_t* focused_window = 0;
 window_t* find_window_by_z(uint32_t z){
     window_t* current = head;
     while(current){
@@ -73,7 +74,7 @@ uint32_t screen_bytes_per_row = 0;
 uint32_t screen_bytespp = 0;
 int wm_to_k_fd = 0;
 int k_to_wm_fd = 0;
-
+uint8_t window_dirty = 0;
 void add_window_to_list(window_t* new_win){
     window_t* curr = head;
     if (!curr) head = new_win;
@@ -103,21 +104,34 @@ void handle_window_request(framebuffer_t* fb_metadata,win_man_msg_t* msg){
     new_win->width = req->width;
     new_win->height = req->height;
     new_win->owner_pid = msg->pid;
+
+    unsigned char* pid_str = uint32_to_ascii(msg->pid);
+    int dir = open(pid_str,FILE_FLAG_CREATE | FILE_CREATE_DIR);
+    chdir(pid_str);
+
+    mknod_params_t params = {
+        .type = TYPE_PIPE,
+        .flags = MNODE_FLAG_PID_DEFINED_PIPE,
+        .read_pid = new_win->owner_pid,
+        .write_pid = getpid()
+    };
+
+    mknod("kb_pipe",&params); // replaces read and write pids with the opened fds
+    new_win->kb_pipe_fd = params.write_pid;
+
     new_win->buffer_size = new_win->width * new_win->height * sizeof(uint32_t);
     new_win->next = 0;
     new_win->z = find_largest_z() + 1;
     new_win->origin_x = (fb_metadata->width / 5);
     new_win->origin_y = (fb_metadata->height / 5);
-    unsigned char* pid_str = uint32_to_ascii(new_win->owner_pid);
-    uint32_t filename_len = sizeof("win_") + strlen(pid_str);
+    uint32_t filename_len = sizeof("win.tmp");
     unsigned char* backing_filename = (unsigned char*)malloc(filename_len);
-    memcpy(backing_filename,"win_",sizeof("win_"));
-    memcpy(&backing_filename[sizeof("win_") - 1],pid_str,strlen(pid_str));
-    backing_filename[filename_len - 1] = '\0';
+    memcpy(backing_filename,"win.tmp",sizeof("win.tmp"));
+    //memcpy(&backing_filename[sizeof("win_") - 1],pid_str,strlen(pid_str));
+    //backing_filename[filename_len - 1] = '\0';
     free(pid_str);
-    chdir("tmp"); // too lazy to parse directories when creating files rn
-    new_win->backing_fd = open(backing_filename,FILE_FLAG_CREATE);
-    chdir("..");
+
+    new_win->backing_fd = open(backing_filename,FILE_FLAG_CREATE | FILE_FLAG_READ | FILE_FLAG_WRITE);
     
     new_win->live_buffer = (unsigned char*)mmap(new_win->buffer_size,PROT_READ | PROT_WRITE,MAP_SHARED,new_win->backing_fd,0);
     new_win->comitted_buffer = (unsigned char*)malloc(new_win->buffer_size);
@@ -125,12 +139,13 @@ void handle_window_request(framebuffer_t* fb_metadata,win_man_msg_t* msg){
     memset(new_win->live_buffer,0,new_win->buffer_size);
     close(new_win->backing_fd);
     
-    window_creation_ans_t creation_ans =
-    {.width = new_win->width,
-            .height = new_win->height, 
-            .filename_len = filename_len,
-            .pid = new_win->owner_pid,
-            .answer_type = DEV_WM_ANS_TYPE_WIN_CREATION,
+    window_creation_ans_t creation_ans = {
+        .width = new_win->width,
+        .height = new_win->height, 
+        .filename_len = filename_len,
+        .pid = new_win->owner_pid,
+        .answer_type = DEV_WM_ANS_TYPE_WIN_CREATION,
+        .kb_fd = params.read_pid,
         };
     // always pass the answer type first
     write(wm_to_k_fd,(unsigned char*)&creation_ans,sizeof(window_creation_ans_t)); 
@@ -138,14 +153,15 @@ void handle_window_request(framebuffer_t* fb_metadata,win_man_msg_t* msg){
     
     add_window_to_list(new_win);
     free(backing_filename);
+    chdir("..");
 }
 
 void handle_window_commit(framebuffer_t* fb_metadata,win_man_msg_t* msg){
-    debug("recieved commit");
     window_t* win = find_window_by_pid(msg->pid);
     if (!win) return;
 
     memcpy(win->comitted_buffer,win->live_buffer,win->buffer_size);
+    window_dirty = 1;
 }
 
 void write_window_to_fb(framebuffer_t* fb_metadata, window_t* win) {
@@ -178,13 +194,15 @@ void write_window_to_fb(framebuffer_t* fb_metadata, window_t* win) {
     }
 
     if (win->origin_y + rows_to_copy < fb_metadata->height) {
-        memset(win_start, 0xAA, width_copy_size); // draw 1px horizontal line
+        memset(win_start, 0xAA, width_copy_size);
     }
 }
 
 
 void composite_windows(framebuffer_t* fb_metadata){
     if (!head) return;
+    if (!window_dirty) return;
+    
     uint32_t current_z = 0; // 0 is guaranteed to not be a Z-layer
     while(1){
         window_t* win = find_window_with_next_larger_z(current_z);
@@ -193,6 +211,11 @@ void composite_windows(framebuffer_t* fb_metadata){
         write_window_to_fb(fb_metadata,win);
         current_z = win->z;
     }
+
+    uint32_t max_z = find_largest_z();
+    focused_window = find_window_by_z(max_z);
+
+    window_dirty = 0;
 }
 
 __attribute__((section(".text.start")))
@@ -201,11 +224,19 @@ void main(){
     int kb0_fd = open("dev/kb0",FILE_FLAG_NONE);
     wm_to_k_fd = open("tmp/wm_to_k.tmp",FILE_FLAG_WRITE);
     k_to_wm_fd = open("tmp/k_to_wm.tmp",FILE_FLAG_READ);
-    if (fb0_fd < 0) exit(1);
+
+    chdir("tmp");
+    int wm_dir = open("wm",FILE_FLAG_CREATE | FILE_CREATE_DIR);
+    if (wm_dir < 0) exit(1);
+    close(wm_dir);
+    chdir("wm");
+
+    if (fb0_fd < 0) exit(2);
+    if (kb0_fd < 0) exit(2);
     if (wm_to_k_fd < 0) exit(2);
-    if (k_to_wm_fd < 0) exit(3);
+    if (k_to_wm_fd < 0) exit(2);
     framebuffer_t fb_metadata;
-    if (ioctl(fb0_fd,DEV_FB0_GET_METADATA,&fb_metadata) < 0) exit(4);
+    if (ioctl(fb0_fd,DEV_FB0_GET_METADATA,&fb_metadata) < 0) exit(3);
     
     screen_bytes_per_row = fb_metadata.bytes_per_row;
     screen_bytespp = fb_metadata.bpp / 8;
@@ -213,26 +244,38 @@ void main(){
     fb0 = (unsigned char*)mmap(fb_metadata.size, PROT_READ | PROT_WRITE, MAP_SHARED, fb0_fd,0);
     
     close(fb0_fd);
-    if (!fb0) exit(5);
+    if (!fb0) exit(4);
     
-    unsigned char buffer[128] = {0};
+    unsigned char buffer[256] = {0};
     
+    uint32_t n_input = 0;
+    uint32_t n_kmsg = 0;
     while(1){
-        memset(buffer,0,sizeof(buffer));        
-        while(read(k_to_wm_fd,buffer,sizeof(buffer)) <= 0){}
-        win_man_msg_t* msg = (win_man_msg_t*)buffer;
-        switch (msg->cmd)
-        {
-            case DEV_WM_REQUEST_WINDOW:{
-                handle_window_request(&fb_metadata,msg);
-                break;
-            }
-            case DEV_WM_COMMIT_WINDOW: {
-                handle_window_commit(&fb_metadata,msg);
-                break;
+        n_input = read(kb0_fd,buffer,sizeof(buffer));
+        if (focused_window && n_input > 0){
+            write(focused_window->kb_pipe_fd,buffer,n_input);
+        }
+
+        memset(buffer,0,sizeof(buffer));
+
+        n_kmsg = read(k_to_wm_fd,buffer,sizeof(buffer));
+        if (n_kmsg > 0){
+
+            win_man_msg_t* msg = (win_man_msg_t*)buffer;
+            switch (msg->cmd)
+            {
+                case DEV_WM_REQUEST_WINDOW:{
+                    handle_window_request(&fb_metadata,msg);
+                    break;
+                }
+                case DEV_WM_COMMIT_WINDOW: {
+                    handle_window_commit(&fb_metadata,msg);
+                    break;
+                }
             }
         }
         composite_windows(&fb_metadata);
+
     }
 
     exit(EXIT_SUCCESS);
