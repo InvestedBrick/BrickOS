abf4dd6973adcfdf6fc1fbf307dbf86645ec895b
InvestedBrick <julianbrecht25@gmail.com>
2025-10-07 19:38:12 +0200

    updated flags

--- src/tables/gdt_setup.c
+++ src/tables/gdt_setup.c
@@ -24,11 +24,11 @@ void init_gdt() {
     //USR EX  1 11  1 1 0  1  0
     //USR DT  1 11  1 0 0  1  0
     set_gdt_entry(0,0,0,0,0); // null segment 0x0
-    set_gdt_entry(1,0,0xffffffff,KERNEL_CODE_SEGMENT,GRANULARITY); // Code segment 0x08
-    set_gdt_entry(2,0,0xffffffff,KERNEL_DATA_SEGMENT,GRANULARITY); // Data segment 0x10
+    set_gdt_entry(1,0,0xffffffff,KERNEL_CODE_SEGMENT,FLAGS); // Code segment 0x08
+    set_gdt_entry(2,0,0xffffffff,KERNEL_DATA_SEGMENT,FLAGS); // Data segment 0x10
 
-    set_gdt_entry(3,0,0xffffffff,USER_CODE_SEGMENT,GRANULARITY); // 0x18
-    set_gdt_entry(4,0,0xffffffff,USER_DATA_SEGMENT,GRANULARITY); // 0x20
+    set_gdt_entry(3,0,0xffffffff,USER_CODE_SEGMENT,FLAGS); // 0x18
+    set_gdt_entry(4,0,0xffffffff,USER_DATA_SEGMENT,FLAGS); // 0x20
 
     write_tss(0x10,0); // 0x10 is the Kernel data segment
 
@@ -37,7 +37,7 @@ void init_gdt() {
 }
 
 void write_tss(uint16_t ss0, uint32_t esp0){
-    set_gdt_entry(5,(uint32_t)&tss_entry,sizeof(tss_entry_t) - 1,0x89,GRANULARITY  & ~(0x80)); // 0x89 = present, ring 0, type 9 (TSS)
+    set_gdt_entry(5,(uint32_t)&tss_entry,sizeof(tss_entry_t) - 1,0x89,FLAGS  & ~(0x80)); // 0x89 = present, ring 0, type 9 (TSS)
     memset(&tss_entry,0,sizeof(tss_entry_t));
     tss_entry.ss0 = ss0;
     tss_entry.esp0 = esp0;
