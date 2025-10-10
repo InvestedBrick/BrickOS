a0b9e0248eaefea8eb44b6bf10d6892511f0df19
InvestedBrick <julianbrecht25@gmail.com>
2025-10-10 17:11:34 +0200

    Adjusted headers and defines for 64 bit

--- src/tables/gdt.h
+++ src/tables/gdt.h
@@ -11,21 +11,37 @@ typedef struct {
     uint16_t limit_low;
     uint16_t base_low;
     uint8_t base_middle;
-    uint8_t access;
-    uint8_t granularity;
+    uint8_t access; // 4 bits limit high, 4 bits flags
+    uint8_t flags;
     uint8_t base_high;
 }__attribute__((packed)) gdt_entry_t;
 
-#define KERNEL_CODE_SEGMENT 0x9a
-#define KERNEL_DATA_SEGMENT 0x92
+typedef struct {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t  base_mid;
+    uint8_t  access;
+    uint8_t  flags; // 4 bits limit high, 4 bits flags
+    uint8_t  base_high;
+    uint32_t base_upper;
+    uint32_t reserved;
+} __attribute__((packed)) gdt_entry64_t; 
 
-#define USER_CODE_SEGMENT 0xfa
-#define USER_DATA_SEGMENT 0xf2
+#define KERNEL_CODE_SEGMENT_ACCESS 0x9a
+#define KERNEL_DATA_SEGMENT_ACCESS 0x92
+
+#define USER_CODE_SEGMENT_ACCESS 0xfa
+#define USER_DATA_SEGMENT_ACCESS 0xf2
+
+#define TSS_ACCESS 0x89
 
 // byte granularity, long mode
-#define FLAGS 0xa0
+#define GDT_CODE_SEG_FLAGS 0xa0
+#define GDT_DATA_SEG_GLAGS 0xc0
 
-#define N_GDT_ENTRIES 6
+#define TSS_FLAGS 0x40 
+
+#define N_GDT_ENTRIES 7
 /**
  * load_gdt - loads global descriptor table
  * @param table A pointer to a gdt
@@ -37,7 +53,7 @@ void load_gdt(const gdt_t* table);
  * load_tss - loads the task state segment
  * @param tss_segment The TSS segment index (should be 0x28)
  */
-void load_tss(uint32_t tss_segment);
+void load_tss(uint16_t tss_segment);
 /**
  * set_gdt_entry:
  * creates a gdt entry with the given parameters
