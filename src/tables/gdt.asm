0e059bca65faaabae9fcda6f60672f421106c2e1
InvestedBrick <julianbrecht25@gmail.com>
2025-10-10 17:11:20 +0200

    adjusted asm for 64 bit

--- src/tables/gdt.asm
+++ src/tables/gdt.asm
@@ -1,31 +1,26 @@
 
 global load_gdt
-section .data
-    flush_cs_ptr: dq flush_cs
-section .text
-
 ; load_gdt: loads a global descriptor table
-; stack:    [esp + 4] address to a gdt struct
-;           [esp    ] return address
-;
+; first arg (rdi) address of the gdt
 load_gdt:
-    mov eax, [esp + 4]
-    lgdt [eax]   ;         offset idx     ti   rpl
-    mov ax, 0x10 ; 0x10 = 00000000 00010 | 0 | 0 0 |
+    mov rax, rdi
+    lgdt [rax]   ;         offset idx     ti   rpl
+    push 0x08
+    lea rax, [rel .reload_segments]
+    push rax
+    retfq ; far return
+.reload_segments:
+    mov ax, 0x10 ; 0x10 = 00000000 00010 | 0 | 0 0 | => data segment
     mov ds, ax   
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    jmp qword [rel flush_cs_ptr] ; load the code segment
-flush_cs:
     ret
 
 global load_tss
 load_tss:
     ; loads the task state segment
-    ; stack: [esp + 4] segment index
-    ;        [esp    ] return address
-    mov eax, [esp + 4]
+    mov ax, di
     ltr ax
     ret
