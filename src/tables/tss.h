ebef093b28a92f4b5b19e7ac3d06050447e2c6c1
InvestedBrick <julianbrecht25@gmail.com>
2025-10-10 17:11:52 +0200

    updated for 64 bit

--- src/tables/tss.h
+++ src/tables/tss.h
@@ -3,25 +3,24 @@
 #define INCLUDE_TSS_H
 
 typedef struct {
-    uint32_t prev_tss;
-    uint32_t esp0;
-    uint32_t ss0;
-    uint32_t esp1;
-    uint32_t ss1;
-    uint32_t esp2;
-    uint32_t ss2;
-    uint32_t cr3;
-    uint32_t eip;
-    uint32_t eflags;
-    uint32_t eax, ecx, edx, ebx;
-    uint32_t esp, ebp, esi, edi;
-    uint32_t es, cs, ss, ds, fs, gs;
-    uint32_t ldt;
-    uint16_t trap;
-    uint16_t iomap_base;
+    uint32_t reserved1;
+    uint64_t rsp0;
+    uint64_t rsp1;
+    uint64_t rsp2;
+    uint64_t reserved2;
+    uint64_t ist1;
+    uint64_t ist2;
+    uint64_t ist3;
+    uint64_t ist4;
+    uint64_t ist5;
+    uint64_t ist6;
+    uint64_t ist7;
+    uint64_t reserved3;
+    uint16_t reserved4;
+    uint16_t iobp;
 } __attribute__((packed)) tss_entry_t;
 
-void write_tss(uint16_t ss0, uint32_t esp0);
-void set_kernel_stack(uint32_t stack);
+void write_tss(uint64_t rsp0);
+void set_kernel_stack(uint64_t stack);
 
 #endif
