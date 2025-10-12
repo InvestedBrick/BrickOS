ea3ec93004dd131e0944ab4605066e1cf043ef23
InvestedBrick <julianbrecht25@gmail.com>
2025-10-12 18:40:20 +0200

    clarified that the returned values are physical

--- src/memory/mem_ops.asm
+++ src/memory/mem_ops.asm
@@ -6,16 +6,13 @@ invalidate:
     invlpg [rax]
     ret
 
-global mem_get_current_page_dir
-
-mem_get_current_page_dir:
+global mem_get_current_pml4_phys
+mem_get_current_pml4_phys:
     mov rax, cr3
-    mov rcx, KERNEL_START
-    add rax, rcx 
     ret
 
-global mem_set_current_page_dir
-mem_set_current_page_dir:
+global mem_set_current_pml4_phys
+mem_set_current_pml4_phys:
     mov rax, rdi
     mov cr3, rax
     ret
