fe1d0fdbb1a60bc59f46fdb0fd2f7d8f68541310
InvestedBrick <julianbrecht25@gmail.com>
2025-11-02 11:53:09 +0100

    moved some functions to assembly

--- iso/modules/cstdlib/stdutils.c
+++ iso/modules/cstdlib/stdutils.c
@@ -1,10 +1,5 @@
 #include "stdutils.h"
 #include "malloc.h"
-uint32_t strlen(const char* str){
-    uint32_t len = 0;
-    while(str[len] != '\0'){len++;}
-    return len;
-}
 
 uint8_t streq(const char* str1, const char* str2){
     uint32_t i = 0;
@@ -18,26 +13,6 @@ uint8_t streq(const char* str1, const char* str2){
 
     return 1;
 
-}
-
-void* memset(void* dest, int val, uint32_t n){
-    unsigned char* p = dest;
-    for(uint32_t i = 0; i < n;i++){
-        p[i] = (uint8_t)val;
-    }
-    return dest;
-}
-
-void* memcpy(void* dest,void* src, uint32_t n){
-    if (n == 0) return dest;
-    unsigned char* _dest = dest;
-    unsigned char* _src = src;
-
-    for (uint32_t i = 0; i < n;i++){
-        _dest[i] = _src[i];
-    }
-
-    return dest;
 }
 
 char* uint32_to_ascii(uint32_t value) {
