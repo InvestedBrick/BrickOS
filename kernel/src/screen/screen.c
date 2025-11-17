7ec96b6c556fd8dcd11815238e49338032ee82ba
InvestedBrick <julianbrecht25@gmail.com>
2025-11-17 21:21:28 +0100

    rewrote framebuffer to use limine framebuffer, currently not working due to needing to update memory

--- kernel/src/screen/screen.c
+++ kernel/src/screen/screen.c
@@ -8,31 +8,10 @@
 #include "../multiboot.h"
 #include "font_bitmaps.h"
 #include <stdint.h>
-
+#include <stddef.h>
+#include "../../../limine/limine.h"
+#include "../kernel_header.h"
 volatile unsigned char* fb = 0;
-
-uint8_t screen_bytespp;
-
-uint8_t r_pos = 0;
-uint8_t r_mask_size = 0;
-
-uint8_t g_pos = 0;
-uint8_t g_mask_size = 0;
-
-uint8_t b_pos = 0;
-uint8_t b_mask_size = 0;
-
-// preparation for multiple screens
-uint32_t gfx_cursor_x = 0;
-uint32_t gfx_cursor_y = 0;
-uint32_t screen_cursor_x_max = 0;
-uint32_t screen_cursor_y_max = 0;
-
-uint32_t screen_bytes_per_row;
-uint32_t screen_origin_x = 0;
-uint32_t screen_origin_y = 0;
-uint32_t screen_width;
-uint32_t screen_height;
 
 void map_framebuffer(uint64_t fb_phys,uint64_t fb_virt, uint32_t fb_size){
 
@@ -42,30 +21,18 @@ void map_framebuffer(uint64_t fb_phys,uint64_t fb_virt, uint32_t fb_size){
     } 
 }
 
-void init_framebuffer(multiboot_info_t* mboot,uint64_t fb_start){
+void init_framebuffer(uint64_t fb_start){
 
-    uint64_t fb_phys = (uint64_t)mboot->framebuffer_addr;
-    uint32_t fb_size = mboot->framebuffer_pitch * mboot->framebuffer_height;
+    uint64_t fb_phys = (uint64_t)limine_data.framebuffer - limine_data.hhdm;
+    uint32_t fb_size = limine_data.framebuffer->pitch * limine_data.framebuffer->height;
 
     fb = (volatile unsigned char*)(fb_start);
-    screen_width             = mboot->framebuffer_width;
-    screen_height            = mboot->framebuffer_height;
-    screen_bytes_per_row     = mboot->framebuffer_pitch;
-    screen_bytespp           = mboot->framebuffer_bpp / 8;
-    r_pos                    = mboot->framebuffer_red_field_pos;
-    r_mask_size              = mboot->framebuffer_red_mask_size;
-    g_pos                    = mboot->framebuffer_green_field_pos;
-    g_mask_size              = mboot->framebuffer_green_mask_size;
-    b_pos                    = mboot->framebuffer_blue_field_pos;
-    b_mask_size              = mboot->framebuffer_blue_mask_size;
 
-    screen_cursor_x_max = screen_width / COLUMNS_PER_CHAR;
-    screen_cursor_y_max = screen_height / ROWS_PER_CHAR;
-
-    fb0.bpp            = mboot->framebuffer_bpp;
-    fb0.bytes_per_row  = mboot->framebuffer_pitch;
-    fb0.height         = mboot->framebuffer_height;
-    fb0.width          = mboot->framebuffer_width;
+    //compacted so that I can ship it compactly to the window manager
+    fb0.bpp            = limine_data.framebuffer->bpp;
+    fb0.bytes_per_row  = limine_data.framebuffer->pitch;
+    fb0.height         = limine_data.framebuffer->height;
+    fb0.width          = limine_data.framebuffer->width;
     fb0.phys_addr      = fb_phys;
     fb0.size           = fb_size;
 
