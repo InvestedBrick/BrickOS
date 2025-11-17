ac068b8ec27b475fb039efe462710f79db5b9209
InvestedBrick <julianbrecht25@gmail.com>
2025-11-17 21:21:44 +0100

    Added limine data struct

--- kernel/src/kernel_header.h
+++ kernel/src/kernel_header.h
@@ -2,7 +2,13 @@
 #ifndef INCLUDE_KERNEL_PROCESS_H
 #define INCLUDE_KERNEL_PROCESS_H
 #include "processes/user_process.h"
+#include "../../limine/limine.h"
+typedef struct {
+    struct limine_framebuffer* framebuffer;
+    uint64_t hhdm;
+}limine_data_t;
 
+extern limine_data_t limine_data;
 extern user_process_t global_kernel_process;
 extern uint8_t dispatched_user_mode;
 void shutdown();
