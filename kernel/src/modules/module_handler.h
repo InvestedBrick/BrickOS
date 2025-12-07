b1ddf3fc520106c88009fac02e973a18858653fd
InvestedBrick <julianbrecht25@gmail.com>
2025-12-07 21:25:40 +0100

    loading limine modules

--- kernel/src/modules/module_handler.h
+++ kernel/src/modules/module_handler.h
@@ -3,6 +3,7 @@
 #define MODULE_HANDLER_H
 
 #include "../multiboot.h"
+#include "../../limine-protocol/include/limine.h"
 typedef struct {
     uint32_t size;
     uint64_t start;
@@ -15,16 +16,15 @@ extern uint32_t module_count;
 
 /**
  * save_module_binaries:
- * Saves the module binaries provided by the boot info and their starts to module_binary_t structs and then lists them in an array called "module_binary_structs"
+ * Saves the module binaries provided by limine and their starts to module_binary_t structs and then lists them in an array called "module_binary_structs"
  * @param boot_info The boot info
  */
-void save_module_binaries(multiboot_info_t* boot_info);
+void save_module_binaries(struct limine_module_response* module_response);
 
 /**
  * write_module_binaries_to_file: 
  * Writes the module binaries to files in the modules/ folders
  * IMPORTANT: This function frees the binaries from memory, so the struct pointer becomes invalid
- * 
  */
 void write_module_binaries_to_file();
 #endif
