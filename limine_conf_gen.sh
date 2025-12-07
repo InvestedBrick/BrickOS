83eb44211c66558d73b332d5da15ce4a9c838b19
InvestedBrick <julianbrecht25@gmail.com>
2025-12-07 18:14:23 +0100

    fixed module loading

--- limine_conf_gen.sh
+++ limine_conf_gen.sh
@@ -3,6 +3,7 @@ touch limine.conf
 cat iso/limine_base.conf > limine.conf
 
 find iso/modules -maxdepth 1 -type f -name '*.bin' -print0 | while IFS= read -r -d '' file; do
-    rel=${file#iso/}         
-    printf '    module_path %s \n' "$rel" >> limine.conf
+    rel=${file#iso/modules/}         
+    printf '    module_path: boot():/modules/%s \n' "$rel" >> limine.conf
+    printf '    module_cmdline: %s \n' "$rel" >> limine.conf
 done
