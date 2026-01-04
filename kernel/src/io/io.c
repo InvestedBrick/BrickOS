ea55a5509d2ec08b06dc44f002397aa97e67e1e6
InvestedBrick <julianbrecht25@gmail.com>
2026-01-04 19:39:18 +0100

    Added I/O for longs

--- kernel/src/io/io.c
+++ kernel/src/io/io.c
@@ -8,13 +8,24 @@ void outw(uint16_t port, uint16_t data){
     asm volatile ("outw %0,%1" : : "a"(data), "Nd"(port));
 }
 
+void outl(uint16_t port,uint32_t data){
+    asm volatile ("outl %0, %1" : : "a"(data), "Nd"(port));
+}
+
 uint8_t inb(uint16_t port){
     uint8_t ret;
     asm volatile ("inb %1,%0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
+
 uint16_t inw(uint16_t port){
     uint16_t ret;
     asm volatile ("inw %1,%0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+uint32_t inl(uint16_t port){
+    uint32_t ret;
+    asm volatile ("inl %1,%0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
