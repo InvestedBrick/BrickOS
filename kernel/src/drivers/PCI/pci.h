c5fc2945eecc76ca8b5bc0a7e3267df794e940db
InvestedBrick <julianbrecht25@gmail.com>
2026-01-05 22:20:23 +0100

    enumerating and storing all pci devices

--- kernel/src/drivers/PCI/pci.h
+++ kernel/src/drivers/PCI/pci.h
@@ -3,9 +3,26 @@
 
 #define PCI_CONFIG_ADDRESS 0xcf8
 #define PCI_DATA 0xcfc
-
+#define PCI_DEV_DOESNT_EXIST_VENDOR_ID 0xffff
+#define PCI_HEADER_FLAG_MF (1 << 0x7)
+#define PCI_CLASS_BRIDGE 0x6
+#define PCI_SUBCLASS_PCI_TO_PCI_BRIDGE 0x4 
+#define PCI_MAX_DEVICES 256
 #include <stdint.h>
+
+typedef struct pci_device {
+    uint8_t bus;
+    uint8_t dev;
+    uint8_t func;
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint8_t class_code;
+    uint8_t subclass;
+    uint8_t header_type;
+} pci_device_t;
 
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 
+
+void check_all_busses();
 #endif
