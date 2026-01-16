8a6a3f34feb0bea23d1b9c8bec4163d48d3b0843
InvestedBrick <julianbrecht25@gmail.com>
2026-01-16 21:59:59 +0100

    slight cleanup

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,5 +1,6 @@
 #include "io/io.h"
 #include "io/log.h"
+#include "ACPI/acpi.h"
 #include "tables/gdt.h"
 #include "kernel_header.h"
 #include "memory/memory.h"
@@ -21,67 +22,11 @@
 #include "drivers/PS2/ps2_controller.h"
 #include "drivers/PS2/keyboard/keyboard.h"
 #include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
-#include "../limine-protocol/include/limine.h"
-
-limine_data_t limine_data;
-
-__attribute__((used, section(".limine_requests")))
-volatile static LIMINE_BASE_REVISION(4);
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_paging_mode_request paging_mode_request = {
-    .id = LIMINE_PAGING_MODE_REQUEST,
-    .revision = 1,
-    .mode = LIMINE_PAGING_MODE_X86_64_4LVL,
-    .min_mode = LIMINE_PAGING_MODE_X86_64_4LVL,
-    .max_mode = LIMINE_PAGING_MODE_X86_64_4LVL,
-};
-
-__attribute__((used,section(".limine_requests")))
-static volatile struct limine_rsdp_request rsdp_request = {
-    .id = LIMINE_RSDP_REQUEST,
-    .revision = 0
-};
-
-__attribute__((used,section(".limine_requests")))
-static volatile struct limine_hhdm_request hhdm_request = {
-    .id = LIMINE_HHDM_REQUEST,
-    .revision = 0,
-};
-
-__attribute__((used,section(".limine_requests")))
-static volatile struct limine_memmap_request memmap_request = {
-    .id = LIMINE_MEMMAP_REQUEST,
-    .revision = 0,
-};
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_boot_time_request date_at_boot_request = {
-    .id = LIMINE_BOOT_TIME_REQUEST,
-    .revision = 0
-};
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0
-};
-
-__attribute((used,section(".limine_requests")))
-static volatile struct limine_module_request module_request = {
-    .id = LIMINE_MODULE_REQUEST,
-    .revision = 0
-};
-
-__attribute__((used, section(".limine_requests_start")))
-static volatile LIMINE_REQUESTS_START_MARKER;
-
-__attribute__((used, section(".limine_requests_end")))
-static volatile LIMINE_REQUESTS_END_MARKER;
 
 struct user_process global_kernel_process;
 uint8_t dispatched_user_mode = 0;
+extern uint8_t check_SSE();
+extern void enable_SSE();
 
 void setup_kernel_fds(){
     global_kernel_process.fd_table[FD_STDIN] = fs_open("dev/kb0",FILE_FLAG_NONE);
@@ -122,6 +67,9 @@ void kmain()
 {   
     uint64_t stack_top;
     asm volatile ("mov %%rsp, %0" : "=r"(stack_top));
+    
+    if (check_SSE()) enable_SSE();
+
     // Serial port setup
     serial_configure_baud_rate(SERIAL_COM1_BASE,3);
     serial_configure_line(SERIAL_COM1_BASE);
@@ -129,42 +77,11 @@ void kmain()
     serial_configure_modem(SERIAL_COM1_BASE);
     log("Set up serial port");
     
-    if (!LIMINE_BASE_REVISION_SUPPORTED){
-        panic("Limine base revision unsupported");
-    }
-
-    if (!rsdp_request.response) warn("No XSDP table given");
-    else {
-        uint64_t rsdp = (uint64_t)rsdp_request.response->address;
-        limine_data.rsdp = rsdp;
-    }
-    if (!hhdm_request.response) panic("No HHDM request response");
-
-    limine_data.hhdm = hhdm_request.response->offset;
-    logf("Kernel mapped at offset: %x",limine_data.hhdm);
-    if (date_at_boot_request.response){
-        limine_data.boot_time = date_at_boot_request.response->boot_time;
-        current_timestamp = limine_data.boot_time; // kernel setup is disregarded as it only takes ~2secs
-        logf("Boot unix timestamp: %d",current_timestamp);
-    }
-
-    if (memmap_request.response == NULL) panic("No memmap recieved");
-    if (module_request.response == NULL) warn("No modules were loaded, likely not intended");
-
-    limine_data.mmap_data.n_entries = memmap_request.response->entry_count;
-    limine_data.mmap_data.memmap_entries = memmap_request.response->entries; 
-
-    if (framebuffer_request.response == NULL 
-     || framebuffer_request.response->framebuffer_count < 1){
-        panic("No Framebuffer");
-    }
-
-    limine_data.framebuffer = framebuffer_request.response->framebuffers[0];
-
+    parse_bootloader_data();
     // Set up global descriptor table
     init_gdt();
     log("Initialized the GDT");
-    
+
     init_pit(DESIRED_STANDARD_FREQ);
     log("Initialized the PIT");
     disable_interrupts(); // We dont want interrupts right now, since we cant correctly return to kernel land once we interrupt
@@ -185,10 +102,13 @@ void kmain()
     init_kmalloc(MEMORY_PAGE_SIZE);
     log("Initialized kmalloc");
 
+    init_acpi();
+    log("Initialized ACPI");
+
     pci_check_all_busses();
     log("Scanned PCI busses");
 
-    save_module_binaries(module_request.response);
+    save_module_binaries(limine_data.modules);
     log("Saved module binaries");
     
     init_shm_obj_vector();
