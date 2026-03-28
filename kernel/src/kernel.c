e9d970066dfc56583f5865ce40ab230932d74e17
InvestedBrick <julianbrecht25@gmail.com>
2026-03-28 21:21:53 +0100

    launching two shells

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -178,7 +178,7 @@ void kmain()
     log("Wrote module binaries to files in the modules directory");
     
     // Everything is now set up
-    //run("modules/terminal.bin",nullptr,nullptr,PRIV_STD);
+    run("modules/terminal.bin",nullptr,nullptr,PRIV_STD);
     run("modules/terminal.bin",nullptr,nullptr,PRIV_STD);
     
     run("modules/win_man.bin",nullptr,nullptr,PRIV_SPECIAL); // window manager should open dev/kb0
