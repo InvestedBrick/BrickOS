9386870506c68648b0996372c27d1e2f3937f881
InvestedBrick <julianbrecht25@gmail.com>
2026-03-28 19:44:02 +0100

    more SSE and FPU stuff

--- kernel/src/misc_cpu.asm
+++ kernel/src/misc_cpu.asm
@@ -1,4 +1,4 @@
-
+.text
 global check_SSE
 check_SSE:
     mov rax, 0x1
@@ -10,7 +10,7 @@ check_SSE:
     jz .noSSE
     mov rax,1
     ret
-.noSSE
+.noSSE:
     xor rax,rax
     ret
 
@@ -25,6 +25,32 @@ enable_SSE:
     mov cr4, rax
     ret
 
+global check_FPU
+check_FPU:
+    mov rdx, cr0
+    and rdx, ~(3 << 2) ;clear cr0.TS and cr0.EM
+    mov cr0, rdx
+    fninit
+    mov ax, 0x1234
+    fnstsw ax
+    cmp ax, 0
+    jne .noFPU
+    mov rax, 1
+    ret
+.noFPU:
+    xor rax,rax
+    ret
+
+.data
+    value_37A dw 0x37a
+.text
+global setup_FPU
+setup_FPU:
+    fldcw [value_37A]
+    mov rdx, cr0
+    or dx, (1 << 5) ; enable cr0.NE => native fpu errors
+    mov cr0, rdx
+    ret
 global write_msr
 write_msr:
     ; rdi = MSR, rsi = low, rdx = high
