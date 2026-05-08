2e18dfebd423850e259f61a189938f1f25269813
InvestedBrick <julianbrecht25@gmail.com>
2026-05-08 19:25:43 +0200

    copying .elf file now

--- limine_conf_gen.sh
+++ limine_conf_gen.sh
@@ -2,7 +2,7 @@ rm limine.conf
 touch limine.conf
 cat iso/limine_base.conf > limine.conf
 
-find iso/modules -maxdepth 1 -type f -name '*.bin' -print0 | while IFS= read -r -d '' file; do
+find iso/modules -maxdepth 1 -type f -name '*.elf' -print0 | while IFS= read -r -d '' file; do
     rel=${file#iso/modules/}         
     printf '    module_path: boot():/modules/%s \n' "$rel" >> limine.conf
     printf '    module_cmdline: %s \n' "$rel" >> limine.conf
