6d4197efc0055c9a599cc57020d6c6290c9a6d95
InvestedBrick <julianbrecht25@gmail.com>
2026-05-24 17:19:37 +0200

    added .qoi image files to be copied

--- limine_conf_gen.sh
+++ limine_conf_gen.sh
@@ -7,3 +7,9 @@ find iso/modules -maxdepth 1 -type f -name '*.elf' -print0 | while IFS= read -r 
     printf '    module_path: boot():/modules/%s \n' "$rel" >> limine.conf
     printf '    module_cmdline: %s \n' "$rel" >> limine.conf
 done
+
+find iso/modules/images -maxdepth 1 -type f -name '*.qoi' -print0 | while IFS= read -r -d '' file; do
+    rel=${file#iso/modules/images/}         
+    printf '    module_path: boot():/modules/images/%s \n' "$rel" >> limine.conf
+    printf '    module_cmdline: %s \n' "$rel" >> limine.conf
+done
