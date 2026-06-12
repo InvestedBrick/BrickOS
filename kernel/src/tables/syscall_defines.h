5161f28719a827c256e0c0572f6339bacaaa7b16
InvestedBrick <julianbrecht25@gmail.com>
2026-06-12 23:09:54 +0200

    renamed alloc page to sys_mmap and added sys_munmap

--- kernel/src/tables/syscall_defines.h
+++ kernel/src/tables/syscall_defines.h
@@ -10,7 +10,7 @@
 #define SYS_WRITE 0x3
 #define SYS_OPEN 0x4
 #define SYS_CLOSE 0x5
-#define SYS_ALLOC_PAGE 0x6
+#define SYS_MMAP 0x6
 #define SYS_GETCWD 0x7
 #define SYS_GETDENTS 0x8
 #define SYS_CHDIR 0x9
@@ -23,6 +23,7 @@
 #define SYS_GETPID 0x10 
 #define SYS_GETTIMEOFDAY 0x11
 #define SYS_SETTIMEZONE 0x12
+#define SYS_MUNMAP 0x13
 
 #define SYSCALL_FAIL -1
 #define SYSCALL_SUCCESS 0
