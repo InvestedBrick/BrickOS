46400fa8012dd2914e40ad1dcba87c5b8a0cc931
InvestedBrick <julianbrecht25@gmail.com>
2026-07-11 17:35:20 +0200

    ethernet headers

--- kernel/src/networking/ethernet.h
+++ kernel/src/networking/ethernet.h
@@ -13,6 +13,23 @@ typedef struct {
     uint16_t type;
 } __attribute__((packed)) ethernet_header_t;
 
+
+/**
+ * ethernet_handle_packet:
+ * Extracts the ethernet header of an incoming network packet and passes the packet down the rest of the network stack
+ * @param data The raw packet data
+ * @param len The length of the packet
+ */
 void ethernet_handle_packet(uint8_t* data, uint32_t len);
 
+/**
+ * ethernet_add_header:
+ * Adds an ethernet header to a ethernet packet by prepending to previous headers
+ * @param data The packet buffer pointer
+ * @param write_off A pointer to a write offset into the buffer (should be just above where the header will be added)
+ * @param dst_mac A uint8_t[6] array of the destination mac address
+ * @param ethertype The ethertype to specify
+ * @return ETH_HDR_RET_SUCCESS on success, an error otherwise
+ */
+uint8_t ethernet_add_header(uint8_t* data, uint32_t* write_off,uint8_t* dst_mac,uint16_t ethertype);
 #endif
