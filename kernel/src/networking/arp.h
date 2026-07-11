5fa46fc1eb8d3a15df6ca37cd3190acf156d4af9
InvestedBrick <julianbrecht25@gmail.com>
2026-07-11 17:34:37 +0200

    ARP headers

---
+++ kernel/src/networking/arp.h
@@ -0,0 +1,64 @@
+#ifndef INCLUDE_ARP_H
+#define INCLUDE_ARP_H
+
+#include <stdint.h>
+
+#define ARP_HTYPE_ETH 0x1
+#define ARP_PTYPE_IPv4 0x0800
+#define ARP_OP_REQ_MAC 0x1
+#define ARP_OP_REPLY_MAC 0x2
+
+#define ARP_MAC_HLEN 6
+#define ARP_IP_PLEN 4
+
+#define ARP_HDR_RET_SUCCESS 0x0
+#define ARP_HDR_RET_DATA_OVERFLOW 0x1
+
+typedef struct {
+    uint16_t htype;
+    uint16_t ptype;
+    uint8_t hlen; // 6
+    uint8_t plen; // 4
+    uint16_t opcode;
+    uint8_t src_mac[6];
+    uint32_t src_ip;
+    uint8_t dst_mac[6];
+    uint32_t dst_ip;
+}__attribute__((packed)) arp_header_t;
+
+typedef struct arp_mac_cache {
+    uint32_t ip_addr;
+    uint8_t mac[6];
+    struct arp_mac_cache* next;
+} arp_mac_cache_t;
+
+/**
+ * arp_add_header:
+ * Adds an ARP header to a ethernet packet by prepending to previous headers
+ * @param data The packet buffer pointer
+ * @param write_off A pointer to a write offset into the buffer (should be just above where the header will be added)
+ * @param opcode The ARP opcode to specify (request or reply)
+ * @param dst_mac A uint8_t[6] array of the destination mac address or nullptr if unknown (for ARP requests)
+ * @param dst_ip The destination IP address to specify
+ * 
+ * @return ARP_HDR_RET_SUCCESS on success, an error otherwise
+ */
+uint8_t arp_add_header(uint8_t* data, uint32_t* write_off,uint16_t opcode, uint8_t* dst_mac,uint32_t dst_ip);
+
+/**
+ * arp_send_request:
+ * Sends an ARP request for the specified IP address
+ *
+ * @param dst_ip The IP address to request the MAC address for
+ */
+void arp_send_request(uint32_t dst_ip);
+
+/**
+ * arp_handle_packet:
+ * Extracts the ARP header of an incoming network packet, may cache the MAC address of the sender, and may send an ARP response if the packet is an ARP request for this machine's IP address
+ * @param data The raw packet data
+ * @param write_off The offset into the packet where the ARP header starts
+ * @param total_len The total length of the packet
+ */
+void arp_handle_packet(uint8_t* data, uint32_t write_off, uint32_t total_len);
+#endif
