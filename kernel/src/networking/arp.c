4ccfd322d2ffd5e157aa3260ae8f02f56cce45c5
InvestedBrick <julianbrecht25@gmail.com>
2026-07-14 20:36:32 +0200

    using interfaces instead of global driver

--- kernel/src/networking/arp.c
+++ kernel/src/networking/arp.c
@@ -4,9 +4,10 @@
 #include "../memory/kmalloc.h"
 #include "../utilities/util.h"
 #include "../processes/scheduler.h"
+#include "ip.h"
 #include "ethernet.h"
 
-uint8_t arp_add_header(uint8_t* data, uint32_t* write_off,uint16_t opcode, uint8_t* dst_mac,uint32_t dst_ip){
+uint8_t arp_add_header(net_interface_t* iface,uint8_t* data, uint32_t* write_off,uint16_t opcode, uint8_t* dst_mac,uint32_t dst_ip){
     if (*write_off < sizeof (arp_header_t)) return ARP_HDR_RET_DATA_OVERFLOW;
 
     arp_header_t* hdr = (arp_header_t*)(data + *write_off - sizeof(arp_header_t));
@@ -15,8 +16,8 @@ uint8_t arp_add_header(uint8_t* data, uint32_t* write_off,uint16_t opcode, uint8
     hdr->hlen = ARP_MAC_HLEN;
     hdr->plen = ARP_IP_PLEN;
     hdr->opcode = switch_endian16(opcode);
-    memcpy(hdr->src_mac,nic_driver->mac_addr,sizeof(hdr->src_mac));
-    hdr->src_ip = switch_endian32(nic_driver->ip_addr);
+    memcpy(hdr->src_mac,iface->mac_addr,sizeof(hdr->src_mac));
+    hdr->src_ip = switch_endian32(iface->ip_addr);
 
     if (dst_mac) memcpy(hdr->dst_mac,dst_mac,sizeof(hdr->dst_mac));
     else memset(hdr->dst_mac,0x0,sizeof(hdr->dst_mac));
@@ -33,14 +34,17 @@ void arp_send_request(uint32_t dst_ip){
     uint8_t* data = (uint8_t*)kmalloc(total_size);
     uint32_t write_off = total_size;
 
-    uint8_t ret = arp_add_header(data,&write_off,ARP_OP_REQ_MAC,nullptr,dst_ip);
-    if (ret != ARP_HDR_RET_SUCCESS) {warnf("Failed to add ARP header to ARP request"); goto cleanup;}
+    route_t* route = route_lookup(dst_ip);
+    if (!route) return ARP_HDR_RET_NO_ROUTE;
+
+    uint8_t ret = arp_add_header(route->iface,data,&write_off,ARP_OP_REQ_MAC,nullptr,dst_ip);
+    if (ret != ARP_HDR_RET_SUCCESS) {warnf("Failed to add ARP header to ARP request (ERR: %x)",ret); goto cleanup;}
 
     uint8_t broadcast_mac[6] = {0xff,0xff,0xff,0xff,0xff,0xff};
-    ret = ethernet_add_header(data,&write_off,broadcast_mac,ETHERTYPE_ARP);
-    if (ret != ETH_HDR_RET_SUCCESS) {warnf("Failed to add ETHERNET header to ARP request"); goto cleanup;}
+    ret = ethernet_add_header(route->iface,data,&write_off,broadcast_mac,ETHERTYPE_ARP);
+    if (ret != ETH_HDR_RET_SUCCESS) {warnf("Failed to add ETHERNET header to ARP request (ERR: %x)",ret); goto cleanup;}
 
-    nic_driver->send(data,total_size);
+    route->iface->send(data,total_size);
 
 cleanup:
     kfree(data);
@@ -53,13 +57,16 @@ void arp_send_response(arp_header_t* arp_hdr){
 
     uint32_t dst_ip = switch_endian32(arp_hdr->src_ip);
 
-    uint8_t ret = arp_add_header(data,&write_off,ARP_OP_REPLY_MAC,arp_hdr->src_mac,dst_ip);
-    if (ret != ARP_HDR_RET_SUCCESS) {warnf("Failed to add ARP header to ARP response"); goto cleanup;}
+    route_t* route = route_lookup(dst_ip);
+    if (!route) return ARP_HDR_RET_NO_ROUTE;
 
-    ret = ethernet_add_header(data,&write_off,arp_hdr->src_mac,ETHERTYPE_ARP);
-    if (ret != ETH_HDR_RET_SUCCESS) {warnf("Failed to add ETHERNET header to ARP response"); goto cleanup;}
+    uint8_t ret = arp_add_header(route->iface,data,&write_off,ARP_OP_REPLY_MAC,arp_hdr->src_mac,dst_ip);
+    if (ret != ARP_HDR_RET_SUCCESS) {warnf("Failed to add ARP header to ARP response (ERR: %x)",ret); goto cleanup;}
 
-    nic_driver->send(data,total_size);
+    ret = ethernet_add_header(route->iface,data,&write_off,arp_hdr->src_mac,ETHERTYPE_ARP);
+    if (ret != ETH_HDR_RET_SUCCESS) {warnf("Failed to add ETHERNET header to ARP response (ERR: %x)",ret); goto cleanup;}
+
+    route->iface->send(data,total_size);
 
 cleanup:
     kfree(data);
@@ -68,15 +75,19 @@ cleanup:
 void arp_cache_mac(arp_header_t* arp_hdr){
     arp_mac_cache_t* cache = (arp_mac_cache_t*)kmalloc(sizeof(arp_mac_cache_t));
     cache->ip_addr = switch_endian32(arp_hdr->src_ip);
+
+    route_t* route = route_lookup(cache->ip_addr);
+    if (!route) return ARP_HDR_RET_NO_ROUTE;
+
     unsigned char* ip_addr_str = ipv4_to_str(cache->ip_addr);
     logf("ARP: Caching MAC for IP %s",ip_addr_str);
     log_MAC(arp_hdr->src_mac);
     kfree(ip_addr_str);
     memcpy(cache->mac,arp_hdr->src_mac,sizeof(cache->mac));
 
-    arp_mac_cache_t* head = nic_driver->arp_cache_head;
+    arp_mac_cache_t* head = route->iface->arp_cache_head;
 
-    if (!head) nic_driver->arp_cache_head = cache;
+    if (!head) route->iface->arp_cache_head = cache;
     else {
         while(head->next) head = head->next;
         head->next = cache;
@@ -84,7 +95,11 @@ void arp_cache_mac(arp_header_t* arp_hdr){
 }
 
 arp_mac_cache_t* arp_cache_contains_ip(uint32_t ip_addr){
-    arp_mac_cache_t* head = nic_driver->arp_cache_head;
+
+    route_t* route = route_lookup(ip_addr);
+    if (!route) return ARP_HDR_RET_NO_ROUTE;
+
+    arp_mac_cache_t* head = route->iface->arp_cache_head;
     while(head){
         if (head->ip_addr == ip_addr) return head;
         head = head->next;
@@ -130,7 +145,11 @@ void arp_handle_packet(uint8_t* data, uint32_t write_off, uint32_t total_len){
     }
     
     uint32_t dst_ip = switch_endian32(arp_hdr->dst_ip);
-    if (dst_ip != nic_driver->ip_addr) return; // not for me
+
+    route_t* route = route_lookup(dst_ip);
+    if (!route) return; // not for this network (how did it get here??)
+
+    if (dst_ip != route->iface->ip_addr) return; // not for me
 
     uint16_t opcode = switch_endian16(arp_hdr->opcode);
     switch (opcode)
