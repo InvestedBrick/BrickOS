45c9a65f795b4f9e72795e520df0614092dc0f21
InvestedBrick <julianbrecht25@gmail.com>
2026-07-29 21:29:20 +0200

    added enum defines of sockets

---
+++ kernel/src/networking/network_defines.h
@@ -0,0 +1,35 @@
+#ifndef INCLUDE_NETWORK_DEFINES_H
+#define INCLUDE_NETWORK_DEFINES_H
+
+#include <stdint.h>
+
+typedef enum {
+    INET_FAM_IPv4,
+    INET_FAM_IPv6,
+}inet_family_e;
+
+typedef enum {
+    SOCKET_TYPE_DGRAM,
+    SOCKET_TYPE_STREAM,
+    SOCKET_TYPE_RAW,
+    SOCKET_TYPE_UNUSED
+}socket_type_e;
+
+typedef enum {
+    SOCKET_INET,
+    SOCKET_UNUSED,
+}socket_domain_e;
+
+typedef struct {
+    uint16_t inet_family;
+    uint16_t inet_port;
+    uint32_t inet_addr;
+    uint8_t pad[8]; // to make it castable to sockaddr_t
+}in_sockaddr_t;
+
+typedef struct {
+    inet_family_e inet_family;
+    uint8_t sa_data[14];
+}sockaddr_t;
+
+#endif
