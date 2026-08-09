dc17957a96dcf7ada464cfa0f6235f4832bf12ef
InvestedBrick <julianbrecht25@gmail.com>
2026-08-09 21:52:14 +0200

    moved defines needed for userspace here, need to cleanly split in the future

--- kernel/src/networking/network_defines.h
+++ kernel/src/networking/network_defines.h
@@ -62,4 +62,85 @@ typedef struct {
 
 #define MSG_DONTWAIT (1 << 0)
 #define MSG_PEEK (1 << 1)
+
+#define ICMP_TYPE_ECHO_REPLY 0
+#define ICMP_TYPE_DEST_UNR_MSG 3
+#define ICMP_TYPE_SRC_QUENCH_MSG 4
+#define ICMP_TYPE_REDIR_MSG 5
+#define ICMP_TYPE_ECHO_MSG 8
+#define ICMP_TYPE_TIME_EXC_MSG 11
+#define ICMP_TYPE_PARAM_PRBLM_MSG 12
+#define ICMP_TYPE_TIMESTMP_MSG 13
+#define ICMP_TYPE_TIMESTMP_REPLY 14
+#define ICMP_TYPE_INFO_REQ_MSG 15
+#define ICMP_TYPE_INFO_REPLY_MSG 16
+
+typedef struct {
+    uint16_t echo_ident;
+    uint16_t echo_seq;
+}__attribute__((packed)) icmp_echo_t;
+
+typedef struct {
+    uint16_t info_ident;
+    uint16_t info_seq;
+}__attribute__((packed)) icmp_info_t;
+
+typedef struct {
+    uint16_t time_stmp_ident;
+    uint16_t time_stmp_seq;
+    uint32_t originate_timestmp;
+    uint32_t recv_timestmp;
+    uint32_t transmit_timestmp;
+}__attribute__((packed)) icmp_timestamp_t;
+
+typedef struct {
+    uint8_t ptr;
+}__attribute__((packed)) icmp_param_problem_t;
+
+typedef struct {
+    uint32_t gateway_ip_addr;
+}__attribute__((packed)) icmp_redir_t;
+
+typedef struct {
+    uint8_t icmp_type;
+    uint8_t icmp_code;
+    uint16_t checksum;
+    union {
+        uint32_t unused;
+        icmp_echo_t echo;
+        icmp_redir_t redir;
+        icmp_param_problem_t param_problem;
+        icmp_timestamp_t timestamp;
+        icmp_info_t info;
+    }un;
+
+}__attribute__((packed)) icmp_header_t;
+
+#define IP_VERSION_4 0x4
+
+#define IP_TOS_DELAY_LOW        (1 << 3)
+#define IP_TOS_THROUGHPUT_HIGH  (1 << 4)
+#define IP_TOS_RELIABILITY_HIGH (1 << 5)
+
+#define IP_TOS_PREC_ROUTINE         0b000
+#define IP_TOS_PREC_PRIORITY        0b001
+#define IP_TOS_PREC_IMMEDIATE       0b010
+#define IP_TOS_PREC_FLASH           0b011
+#define IP_TOS_PREC_FLASH_OVERRIDE  0b100
+#define IP_TOS_PREC_CRITIC_ECP      0b101
+#define IP_TOS_PREC_INTERNETWORK    0b110
+#define IP_TOS_PREC_NETWORK_CONTROL 0b111
+
+typedef struct {
+    uint8_t version_ihl; // 4 bits version, 4 bits internet header length (in 32 bit words)
+    uint8_t type_of_service;
+    uint16_t total_length; // len of data in 8-bit bytes
+    uint16_t ident; // chosen by sender
+    uint16_t flags_fragment_offset; // 3 bits flags, 13 bits fragment offset
+    uint8_t time_to_live;
+    uint8_t protocol;
+    uint16_t header_checksum;
+    uint32_t src_ip;
+    uint32_t dst_ip;
+}__attribute__((packed)) ipv4_header_t;
 #endif
