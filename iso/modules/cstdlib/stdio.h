b4bcb5952dcf40213387c8eab71e9ebaac1a05ea
InvestedBrick <julianbrecht25@gmail.com>
2026-08-18 14:27:21 +0200

    renamed folder

--- iso/modules/cstdlib/stdio.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef INCLUDE_STDIO_H
-#define INCLUDE_STDIO_H
-
-#include <stdint.h>
-void print(const char* str);
-void print_uint(uint32_t num);
-int read_input(char* buffer,uint32_t buffer_size);
-#endif
