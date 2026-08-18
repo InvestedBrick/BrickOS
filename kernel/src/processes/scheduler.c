2a457fb3c293684465699e81379c4955c49e7028
InvestedBrick <julianbrecht25@gmail.com>
2026-08-18 14:14:50 +0200

    a lot of small fixes

--- kernel/src/processes/scheduler.c
+++ kernel/src/processes/scheduler.c
@@ -13,6 +13,8 @@
 #include "../ACPI/apic.h"
 #include "../tables/timer_callbacks.h"
 #include "spinlocks.h"
+#include "../tables/gdt.h"
+#include "kworker.h"
 #include <stdbool.h>
 
 thread_t* t_queue;
@@ -23,7 +25,7 @@ uint8_t first_switch = 1;
 extern uint32_t stack_top;
 
 void yield(){
-    if (!get_interrupt_status()) return;
+    if (!interrupts_enabled()) return;
 
     asm volatile ("int $" STR(INT_YIELD));
 }
@@ -94,10 +96,9 @@ void init_scheduler(){
     t_queue = global_kernel_process.main_thread;
     current_thread = t_queue;
 
-    // timer callbacks
-    kworker_timer_callbacks = create_kernel_worker_thread(kthread_worker_timer_callbacks);
-
-    init_vector(&timer_callbacks_vec);
+    for (uint32_t i = 0; i < N_KWORKERS; i++){
+        add_kworker();
+    }
 }
 
 thread_t* get_thread_by_tid(uint32_t tid){
@@ -116,7 +117,7 @@ thread_t* create_thread(process_t* owner_proc){
     }
     thread_t* thread = (thread_t*)kmalloc(sizeof(thread_t));
     memset(thread,0x0,sizeof(thread_t));
-    thread->next = 0;
+    thread->next = nullptr;
     
     int tid = get_pid(); // need to put it here so that compiler does not generate weird opcode for some reason
     if (tid == -1) {error("Failed to assign thread id"); return nullptr;}
@@ -162,13 +163,15 @@ int add_thread(struct process* usr_proc){
 thread_t* create_kernel_worker_thread(void (*entry_func)()){
     thread_t* kthread  = create_thread(&global_kernel_process);
     if (!kthread) return nullptr;
-
-    memset(&kthread->regs,0x0,sizeof(kthread->regs));
+    memset(kthread,0x0,sizeof(thread_t));
 
     kthread->regs.rip = (uint64_t)entry_func;
+    kthread->regs.rflags = EFLAGS_IF;
+    kthread->regs.cs = KERNEL_CS;
+    kthread->active_dir = get_inode_by_id(FS_ROOT_DIR_ID);
     uint64_t phys = pmm_alloc_page_frame();
     kthread->init_rsp = map_somewhere_rw(phys) + MEMORY_PAGE_SIZE; // one page, use it wisely
-
+    kthread->owner_proc = &global_kernel_process;
     enqueue_thread(kthread);
 
     kthread->exec_state = EXEC_STATE_FINALIZED;
@@ -180,13 +183,9 @@ thread_t* find_schedule_candidate(){
     thread_t* candidate = current_thread;
     do {
         
-        thread_t* dead_thread = nullptr;
         
-        // cannot kill current thread since then we might no longer have a valid address space
-        if (candidate->exec_state == EXEC_STATE_DEAD && candidate != current_thread) dead_thread = candidate; 
         if (candidate->next) candidate = candidate->next;
         else candidate = t_queue;
-        if (dead_thread) remove_thread(dead_thread);
 
         if (!t_queue->next->next) {
             //TODO: install actually correct shutdown procedure
@@ -200,16 +199,14 @@ thread_t* find_schedule_candidate(){
 }
 
 void switch_task(interrupt_stack_frame_t* regs){
+    
+    uint32_t f = irq_save();
+    
     // only switch when the scheduler was set up 
-    if (!t_queue) return;
-
-    uint8_t int_status = get_interrupt_status();
-    disable_interrupts();
-    if (!t_queue->next){
-        set_interrupt_status(int_status);
+    if (!t_queue || !t_queue->next){
+        irq_restore(f);
         return;
     }
-    
     thread_t* old_thread = current_thread;
     old_thread->kernel_rsp = (uint64_t)regs;
 
@@ -219,14 +216,15 @@ void switch_task(interrupt_stack_frame_t* regs){
 
     current_thread = find_schedule_candidate();
 
-    set_interrupt_status(int_status);
+    irq_restore(f);
 
     if (old_thread->owner_proc->process_id != current_thread->owner_proc->process_id){
         set_kernel_stack(current_thread->owner_proc->kernel_stack_top);
         mem_set_current_pml4_table(current_thread->owner_proc->pml4);
     }
     // send EOI since we cant return to interrupt_handler
-    write_apic_register(APIC_REG_EOI,0x0); 
+    if (regs->interrupt_number != INT_YIELD)
+        write_apic_register(APIC_REG_EOI,0x0); 
 
     if (current_thread->exec_state == EXEC_STATE_FINALIZED){
         current_thread->exec_state = EXEC_STATE_RUNNING;
@@ -248,8 +246,7 @@ void switch_task(interrupt_stack_frame_t* regs){
 
 void remove_thread(thread_t* thread){
     if (!thread) return;
-    uint8_t status = get_interrupt_status();
-    disable_interrupts();
+    uint32_t f = irq_save();
 
     thread_t* before_thread = t_queue;
     while(before_thread->next && before_thread->next != thread) before_thread = before_thread->next;
@@ -265,6 +262,7 @@ void remove_thread(thread_t* thread){
         }
 
         kfree(thread);
+        irq_restore(f);
         return;
     }
 
@@ -276,5 +274,5 @@ void remove_thread(thread_t* thread){
     }
 
     kfree(thread);
-    set_interrupt_status(status);
+    irq_restore(f);
 }
