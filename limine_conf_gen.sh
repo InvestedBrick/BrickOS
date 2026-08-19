4c9aa6e9dc5420537454e4f151eadb8ef25022d0
InvestedBrick <julianbrecht25@gmail.com>
2026-08-19 14:17:35 +0200

    updated path to userland

--- limine_conf_gen.sh
+++ limine_conf_gen.sh
@@ -1,15 +1,15 @@
 rm limine.conf
 touch limine.conf
-cat iso/limine_base.conf > limine.conf
+cat userland/limine_base.conf > limine.conf
 
-find iso/modules -maxdepth 1 -type f -name '*.elf' -print0 | while IFS= read -r -d '' file; do
-    rel=${file#iso/modules/}         
+find userland/modules -maxdepth 1 -type f -name '*.elf' -print0 | while IFS= read -r -d '' file; do
+    rel=${file#userland/modules/}         
     printf '    module_path: boot():/modules/%s \n' "$rel" >> limine.conf
     printf '    module_cmdline: %s \n' "$rel" >> limine.conf
 done
 
-find iso/modules/images -maxdepth 1 -type f -name '*.qoi' -print0 | while IFS= read -r -d '' file; do
-    rel=${file#iso/modules/images/}         
+find userland/modules/images -maxdepth 1 -type f -name '*.qoi' -print0 | while IFS= read -r -d '' file; do
+    rel=${file#userland/modules/images/}         
     printf '    module_path: boot():/modules/images/%s \n' "$rel" >> limine.conf
     printf '    module_cmdline: %s \n' "$rel" >> limine.conf
 done
